f567a0b1a7e4f4c8ff26fcd6af8594e534ad2db8
Ida Onshus <Ida.Onshus@nav.no>
2018-06-29 12:01:25 +0200

    lagt til layout for behandling av stillingen

--- src/stilling/processing/Processing.js
+++ src/stilling/processing/Processing.js
@@ -4,19 +4,27 @@ import { Undertittel } from 'nav-frontend-typografi';
 import { Column, Row } from 'nav-frontend-grid';
 import { Panel } from 'nav-frontend-paneler'
 import { Hovedknapp, Knapp } from 'nav-frontend-knapper';
+import  EtikettBase  from 'nav-frontend-etiketter';
 
 export default function Processing() {
     return (
         <Panel border className="detail-section">
             <Undertittel className="detail-section__head">Behandling av stillingen</Undertittel>
-            <Row>
-                <Column xs="12">
-                    <Hovedknapp
-                    >
-                        Publiser stillingen
-                    </Hovedknapp>
-                </Column>
-            </Row>
+            <EtikettBase type='fokus' className="blokk-s">
+                Stillingen er ikke publisert
+            </EtikettBase>
+            <Hovedknapp className="blokk-xxs"
+            >
+                Publiser stillingen
+            </Hovedknapp>
+            <Hovedknapp className="blokk-xxs"
+            >
+                Avvis stillingen
+            </Hovedknapp>
+            <Hovedknapp className="blokk-xxs"
+            >
+                Neste
+            </Hovedknapp>
         </Panel>
     );
 }
