e86b0d907dc4e55edce794754ff7710b933017b8
Ida Onshus <Ida.Onshus@nav.no>
2018-06-29 08:50:53 +0200

    [PAM-1006]: arbeid på blokk med stillingsbeskrivelse

--- src/stilling/adText/AdText.js
+++ src/stilling/adText/AdText.js
@@ -1,20 +1,43 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import ReactHtmlParser from 'react-html-parser';
-import { Innholdstittel } from 'nav-frontend-typografi';
-import { Panel } from 'nav-frontend-paneler'
+import { Undertittel, Normaltekst } from 'nav-frontend-typografi';
+import { Panel } from 'nav-frontend-paneler';
+import { Input } from 'nav-frontend-skjema';
+import RichTextEditor from 'react-rte';
 import './AdText.less';
 
+const toolbarConfig = {
+    display: ['INLINE_STYLE_BUTTONS', 'BLOCK_TYPE_BUTTONS', 'HISTORY_BUTTONS'],
+    INLINE_STYLE_BUTTONS: [
+        { label: 'Fet', style: 'BOLD', className: 'custom-css-class' },
+        { label: 'Kursiv', style: 'ITALIC' }
+    ],
+    BLOCK_TYPE_BUTTONS: [
+        { label: 'Punkt', style: 'unordered-list-item' }
+    ]
+};
+
+
+
 export default function AdText({ title, adText }) {
+
     if (adText) {
         return (
             <Panel border className="detail-section">
-                <Innholdstittel className="detail-section__head">
-                    {title}
-                </Innholdstittel>
-                <section className="AdText blokk-s">
-                    {ReactHtmlParser(adText)}
-                </section>
+                <Undertittel className="detail-section__head">Stillingsbeskrivelse</Undertittel>
+                <Input
+                    label={"Overskrift på annonsen *"}
+                    defaultValue={title}/>
+                <Input
+                    label={"Stilling / yrke *"}
+                    placeholder='Få konseptnavn og STYRK-08 fra ontologi'/>
+                <Normaltekst>Annonsetekst *</Normaltekst>
+                <RichTextEditor
+                    toolbarConfig={toolbarConfig}
+                    className="rich-text-editor"
+                    value={RichTextEditor.createValueFromString(adText, 'html')}
+                />
             </Panel>
         );
     }
