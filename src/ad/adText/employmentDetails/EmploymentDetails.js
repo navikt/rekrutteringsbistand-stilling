3a3a78322258509612d803b2ee58c647ae4d1a0a
Ida Onshus <Ida.Onshus@nav.no>
2018-07-03 16:29:37 +0200

    la til flere details

--- src/ad/adText/employmentDetails/EmploymentDetails.js
+++ src/ad/adText/employmentDetails/EmploymentDetails.js
@@ -8,6 +8,10 @@ export default function EmploymentDetails({ properties }) {
         <div className="detail-section">
             <Element className="detail-section__head">Om stillingen</Element>
             <dl className="dl-flex typo-normal">
+                {properties.jobtitle && [
+                    <dt key="dt">Stillingstittel:</dt>,
+                    <dd key="dd">{properties.jobtitle}</dd>]
+                }
                 {properties.location && [
                     <dt key="dt">Arbeidssted:</dt>,
                     <dd key="dd">{properties.location}</dd>
@@ -16,6 +20,38 @@ export default function EmploymentDetails({ properties }) {
                     <dt key="dt">Ansettelsesform:</dt>,
                     <dd key="dd">{properties.engagementtype }</dd>
                 ]}
+                {properties.extent && [
+                    <dt key="dt">Heltid/deltid:</dt>,
+                    <dd key="dd">{properties.extent}</dd>
+                ]}
+                {properties.positioncount && [
+                    <dt key="dt">Antall stillinger:</dt>,
+                    <dd key="dd">{properties.positioncount}</dd>
+                ]}
+                {properties.sector && [
+                    <dt key="dt">Sektor:</dt>,
+                    <dd key="dd">{properties.sector}</dd>
+                ]}
+                {properties.workday && [
+                    <dt key="dt">Arbeidsdager:</dt>,
+                    <dd key="dd">{properties.workday}</dd>
+                ]}
+                {properties.workhours && [
+                    <dt key="dt">Arbeidstid:</dt>,
+                    <dd key="dd">{properties.workhours}</dd>
+                ]}
+                {properties.jobarrangement && [
+                    <dt key="dt">Arb.tidsordning:</dt>,
+                    <dd key="dd">{properties.jobarrangement}</dd>
+                ]}
+                {properties.starttime && [
+                    <dt key="dt">Oppstart:</dt>,
+                    <dd key="dd">
+                        {isValidISOString(properties.starttime) ?
+                            formatISOString(properties.starttime, 'D. MMMM YYYY') :
+                            properties.starttime}
+                    </dd>
+                ]}
             </dl>
         </div>
     );
@@ -23,8 +59,16 @@ export default function EmploymentDetails({ properties }) {
 
 EmploymentDetails.propTypes = {
     properties: PropTypes.shape({
+        jobtitle: PropTypes.string,
         location: PropTypes.string,
         engagementtype: PropTypes.string,
+        extent: PropTypes.string,
+        positioncount: PropTypes.string,
+        sector: PropTypes.string,
+        workday: PropTypes.string,
+        workhours: PropTypes.string,
+        jobarrangement: PropTypes.string,
+        starttime: PropTypes.string
     }).isRequired
 };
 
