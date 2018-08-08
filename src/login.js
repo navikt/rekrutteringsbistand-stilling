4ade93a08fa35bdbb349ee85a2ce8b7b408efedd
Tuan Tran <dinh.anh.tuan.tran@nav.no>
2018-08-08 08:43:19 +0200

    fix redirect uri

--- src/login.js
+++ src/login.js
@@ -2,5 +2,5 @@ import { LOGIN_URL } from './fasitProperties';
 
 export const redirectToLogin = () => {
     window.location.href =
-        LOGIN_URL + '?redirectUri=' + window.location.href;
+        LOGIN_URL + '?redirect=' + window.location.href;
 };
