700ba93647d606281608344e0cfecae26162aaeb
Ola Toft-Eriksen <ola.toft-eriksen@nav.no>
2018-08-09 14:38:14 +0200

    Fikser issue med neste annonse og history

--- src/ad/administration/Administration.js
+++ src/ad/administration/Administration.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 import AdminStatusEnum from './AdminStatusEnum';
 import AdStatusEnum from './AdStatusEnum';
 import AdStatusPreview from './AdStatusPreview';
@@ -11,29 +12,49 @@ import CommentsPreview from './CommentsPreview';
 import AdminStatusPreview from './AdminStatusPreview';
 import AdminStatusEdit from './AdminStatusEdit';
 import './Administration.less';
+import { FETCH_AD } from '../adReducer';
 
-function Administration({ adminStatus, adStatus }) {
-    return (
-        <div className="Administration">
-            <AdminStatusPreview />
-            <AdminStatusEdit />
+class Administration extends React.Component {
+    componentDidMount() {
+        const { adUuid } = this.props;
+        const uuidHasChangedInUrl = adUuid !== this.props.match.params.uuid;
+        if (uuidHasChangedInUrl) {
+            this.props.history.push(`/ads/${adUuid}`);
+        }
+    }
 
-            {adminStatus === AdminStatusEnum.PENDING ? (
-                <div>
-                    <AdStatusEdit />
-                    {adStatus === AdStatusEnum.REJECTED && (
-                        <RemarksEdit />
-                    )}
-                    <CommentsEdit />
-                </div>
-            ) : (
-                <div>
-                    <AdStatusPreview />
-                    <CommentsPreview />
-                </div>
-            )}
-        </div>
-    );
+    componentDidUpdate() {
+        const { adUuid } = this.props;
+        const uuidHasChangedInUrl = adUuid !== this.props.match.params.uuid;
+        if (uuidHasChangedInUrl) {
+            this.props.getStilling(this.props.match.params.uuid);
+        }
+    }
+
+    render() {
+        const { adStatus, adminStatus } = this.props;
+        return (
+            <div className="Administration">
+                <AdminStatusPreview />
+                <AdminStatusEdit />
+
+                {adminStatus === AdminStatusEnum.PENDING ? (
+                    <div>
+                        <AdStatusEdit />
+                        {adStatus === AdStatusEnum.REJECTED && (
+                            <RemarksEdit />
+                        )}
+                        <CommentsEdit />
+                    </div>
+                ) : (
+                    <div>
+                        <AdStatusPreview />
+                        <CommentsPreview />
+                    </div>
+                )}
+            </div>
+        );
+    }
 }
 
 Administration.defaultProps = {
@@ -42,12 +63,19 @@ Administration.defaultProps = {
 
 Administration.propTypes = {
     adStatus: PropTypes.string.isRequired,
-    adminStatus: PropTypes.string
+    adminStatus: PropTypes.string,
+    adUuid: PropTypes.string.isRequired,
+    getStilling: PropTypes.func.isRequired
 };
 
 const mapStateToProps = (state) => ({
     adStatus: state.ad.data.status,
-    adminStatus: state.ad.data.administration.status
+    adminStatus: state.ad.data.administration.status,
+    adUuid: state.ad.data.uuid
 });
 
-export default connect(mapStateToProps)(Administration);
+const mapDispatchToProps = (dispatch) => ({
+    getStilling: (uuid) => dispatch({ type: FETCH_AD, uuid })
+});
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Administration));
