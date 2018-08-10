7f1d376950239c87fcb3ecf83dbd0399566390fa
Ola Toft-Eriksen <ola.toft-eriksen@nav.no>
2018-08-10 15:27:43 +0200

    Gjør om toppmeny til sidemeny og rydder opp på forsiden

--- src/startPage/StartPage.js
+++ src/startPage/StartPage.js
@@ -1,34 +1,66 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { Hovedknapp } from 'nav-frontend-knapper';
+import { Normaltekst, Sidetittel } from 'nav-frontend-typografi';
 import { Column, Container, Row } from 'nav-frontend-grid';
-import { Sidetittel, Systemtittel } from 'nav-frontend-typografi';
 import './StartPage.less';
-import Menu from './menu/Menu';
 import { removeShortcuts } from '../common/shortcuts/Shortcuts';
+import { FETCH_NEXT_AD } from '../ad/adReducer';
 
 
 class StartPage extends React.Component {
+    componentDidUpdate() {
+        if (this.props.adUuid) {
+            this.props.history.push(`/ads/${this.props.adUuid}`);
+        }
+    }
+
     componentWillUnmount() {
         removeShortcuts('forside');
     }
 
+    onNextClick = () => {
+        this.props.getNextAd();
+    };
+
     render() {
         return (
             <Container className="StartPage">
-                <Row className="StartPage__settings blokk-m">
-                    <Link className="StartPage__settings__link" to="nav.no">
-                        <Systemtittel>
-                            <i>⚙</i>
-                            Innstillinger
-                        </Systemtittel>
-                    </Link>
-                </Row>
                 <Row>
-                    <Sidetittel className="blokk-s">Annonsemottak</Sidetittel>
-                    <Menu history={this.props.history}/>
+                    <Column xs="12" md="4" />
+                    <Column xs="12" md="4">
+                        <Sidetittel className="StartPage-sidetittel">Annonsemottak</Sidetittel>
+                        <Hovedknapp
+                            onClick={this.onNextClick}
+                            className="AdminStatusEdit__button"
+                        >
+                            Start med neste ledige annonse
+
+                        </Hovedknapp>
+                    </Column>
                 </Row>
             </Container>
         );
     }
 }
-export default StartPage;
+
+StartPage.defaultProps = {
+    adUuid: undefined
+};
+
+StartPage.propTypes = {
+    history: PropTypes.shape().isRequired,
+    getNextAd: PropTypes.func.isRequired,
+    adUuid: PropTypes.string
+};
+
+const mapStateToProps = (state) => ({
+    adUuid: state.ad.data ? state.ad.data.uuid : undefined
+});
+
+const mapDispatchToProps = (dispatch) => ({
+    getNextAd: () => dispatch({ type: FETCH_NEXT_AD })
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(StartPage);
