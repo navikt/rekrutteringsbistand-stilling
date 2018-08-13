412030530a7820147c4190473935b5a3cbc356ed
erlendjohansen <erlend.johansen@nav.no>
2018-08-13 15:53:56 +0200

    Endrer redigering av annonse til å være på 1 kolonne. Legger kategorier inni ekspanderbare panel. Endrer også på rekkefølgen på kategoriene.

--- src/ad/edit/Edit.js
+++ src/ad/edit/Edit.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Column, Row } from 'nav-frontend-grid';
+import { Row } from 'nav-frontend-grid';
 import {
-    Input, SkjemaGruppe, Textarea
+    Input, Textarea
 } from 'nav-frontend-skjema';
+import Ekspanderbartpanel from 'nav-frontend-ekspanderbartpanel';
 import { connect } from 'react-redux';
 import {
     SET_AD_TEXT,
@@ -127,173 +128,169 @@ class Edit extends React.Component {
         return (
             <div className="Edit">
                 <Row>
-                    <Column xs="12" md="8">
-                        <div className="Edit__body">
+                    <div>
+                        <Input
+                            label="Tittel"
+                            value={ad.title}
+                            onChange={this.onTitleChange}
+                            className="typo-normal Edit__title"
+                            feil={validation.title ? { feilmelding: validation.title } : undefined}
+                        />
+                        <Textarea
+                            label=""
+                            onChange={this.onAdTextChange}
+                            value={ad.properties.adtext || ''}
+                        />
+                    </div>
+                    <div className="Edit__details">
+                        <Ekspanderbartpanel className="Edit__panel" tittel="STYRK-kode" tittelProps="undertittel" border="true" apen="true">
+                            <Styrk />
+                        </Ekspanderbartpanel>
+                        <Ekspanderbartpanel className="Edit__panel" tittel="Sted" tittelProps="undertittel" border="true" apen="true">
                             <Input
-                                label="Tittel"
-                                value={ad.title}
-                                onChange={this.onTitleChange}
-                                className="typo-normal Edit__title"
-                                feil={validation.title ? { feilmelding: validation.title } : undefined}
+                                label="Gateadresse"
+                                value={ad.location && ad.location.address ? ad.location.address : ''}
+                                onChange={this.onAddressChange}
+                                className="typo-normal"
                             />
-                            <Textarea
-                                label=""
-                                onChange={this.onAdTextChange}
-                                value={ad.properties.adtext || ''}
+                            <PostalCode />
+                        </Ekspanderbartpanel>
+                        <Ekspanderbartpanel className="Edit__panel" tittel="Søknad" tittelProps="undertittel" border="true">
+                            <Input
+                                label="Søknadsfrist"
+                                value={ad.properties.applicationdue || ''}
+                                onChange={this.onApplicationDueChange}
+                                className="typo-normal"
                             />
-                        </div>
-                    </Column>
-                    <Column xs="12" md="4">
-                        <div className="Edit__details">
-                            <SkjemaGruppe title="STYRK-kode">
-                                <Styrk />
-                            </SkjemaGruppe>
-                            <SkjemaGruppe title="Søknad">
-                                <Input
-                                    label="Søknadsfrist"
-                                    value={ad.properties.applicationdue || ''}
-                                    onChange={this.onApplicationDueChange}
-                                    className="typo-normal"
-                                />
-                                <Input
-                                    label="Send søknad til"
-                                    value={ad.properties.applicationemail || ''}
-                                    onChange={this.onApplicationEmailChange}
-                                    className="typo-normal"
-                                />
-                                <Input
-                                    label="Søknadslenke"
-                                    value={ad.properties.applicationurl || ''}
-                                    onChange={this.onApplicationUrlChange}
-                                    className="typo-normal"
-                                />
-                                <Input
-                                    label="Kildelenke"
-                                    value={ad.properties.sourceurl || ''}
-                                    onChange={this.onSourceUrlChange}
-                                    className="typo-normal"
-                                />
-                            </SkjemaGruppe>
-                            <SkjemaGruppe title="Om stillingen">
-                                <Input
-                                    label="Stillingstittel"
-                                    value={ad.properties.jobtitle || ''}
-                                    onChange={this.onJobtitleChange}
-                                    className="typo-normal"
-                                />
-                                <Input
-                                    label="Arbeidssted"
-                                    value={ad.properties.location || ''}
-                                    onChange={this.onLocationChange}
-                                    className="typo-normal"
-                                />
-                                <EngagementType />
-                                <Input
-                                    label="Heltid/deltid"
-                                    value={ad.properties.extent || ''}
-                                    onChange={this.onExtentChange}
-                                    className="typo-normal"
-                                />
-                                <Input
-                                    label="Antall stillinger"
-                                    value={ad.properties.positioncount || ''}
-                                    onChange={this.onPositioncountChange}
-                                    className="typo-normal"
-                                />
-                                <Input
-                                    label="Sektor"
-                                    value={ad.properties.sector || ''}
-                                    onChange={this.onSectorChange}
-                                    className="typo-normal"
-                                />
-                                <Input
-                                    label="Arbeidsdager"
-                                    value={ad.properties.workday || ''}
-                                    onChange={this.onWorkdayChange}
-                                    className="typo-normal"
-                                />
-                                <Input
-                                    label="Arbeidstid"
-                                    value={ad.properties.workhours || ''}
-                                    onChange={this.onWorkhoursChange}
-                                    className="typo-normal"
-                                />
-                                <Input
-                                    label="Arb.tidordning"
-                                    value={ad.properties.jobarrangement || ''}
-                                    onChange={this.onJobarrangementChange}
-                                    className="typo-normal"
-                                />
-                                <Input
-                                    label="Oppstart"
-                                    value={ad.properties.starttime || ''}
-                                    onChange={this.onStarttimeChange}
-                                    className="typo-normal"
-                                />
-                            </SkjemaGruppe>
-                            <SkjemaGruppe title="Sted">
-                                <Input
-                                    label="Gateadresse"
-                                    value={ad.location && ad.location.address ? ad.location.address : ''}
-                                    onChange={this.onAddressChange}
-                                    className="typo-normal"
-                                />
-                                <PostalCode />
-                            </SkjemaGruppe>
-                            <SkjemaGruppe title="Om arbeidsgiver">
-                                <Input
-                                    label="Arbeidsgiver"
-                                    value={ad.properties.employer || ''}
-                                    onChange={this.onEmployerChange}
-                                    className="typo-normal"
-                                />
-                                <Input
-                                    label="Arbeidsgiver"
-                                    value={ad.properties.employerdescription || ''}
-                                    onChange={this.onEmployerDescriptionChange}
-                                    className="typo-normal"
-                                />
-                            </SkjemaGruppe>
-                            <SkjemaGruppe title="Om annonsen">
-                                <Input
-                                    label="Publisert"
-                                    value={ad.published || ''}
-                                    onChange={this.onPublishedChange}
-                                    className="typo-normal"
-                                />
-                                <Input
-                                    label="Sist endret"
-                                    value={ad.updated || ''}
-                                    onChange={this.onLastUpdatedChange}
-                                    className="typo-normal"
-                                />
-                                <Input
-                                    label="Hentet fra"
-                                    value={ad.medium || ''}
-                                    onChange={this.onMediumChange}
-                                    className="typo-normal"
-                                />
-                                <Input
-                                    label="Løpenr"
-                                    value={ad.id || ''}
-                                    onChange={this.onIdChange}
-                                    className="typo-normal"
-                                />
-                                <Input
-                                    label="Referanse"
-                                    value={ad.reference || ''}
-                                    onChange={this.onReferenceChange}
-                                    className="typo-normal"
-                                />
-                                <Input
-                                    label="Utløpsdato"
-                                    value={ad.expires || ''}
-                                    onChange={this.onExpiresChange}
-                                    className="typo-normal"
-                                />
-                            </SkjemaGruppe>
-                        </div>
-                    </Column>
+                            <Input
+                                label="Send søknad til"
+                                value={ad.properties.applicationemail || ''}
+                                onChange={this.onApplicationEmailChange}
+                                className="typo-normal"
+                            />
+                            <Input
+                                label="Søknadslenke"
+                                value={ad.properties.applicationurl || ''}
+                                onChange={this.onApplicationUrlChange}
+                                className="typo-normal"
+                            />
+                            <Input
+                                label="Kildelenke"
+                                value={ad.properties.sourceurl || ''}
+                                onChange={this.onSourceUrlChange}
+                                className="typo-normal"
+                            />
+                        </Ekspanderbartpanel>
+                        <Ekspanderbartpanel className="Edit__panel" tittel="Om stillingen" tittelProps="undertittel" border="true">
+                            <Input
+                                label="Stillingstittel"
+                                value={ad.properties.jobtitle || ''}
+                                onChange={this.onJobtitleChange}
+                                className="typo-normal"
+                            />
+                            <Input
+                                label="Arbeidssted"
+                                value={ad.properties.location || ''}
+                                onChange={this.onLocationChange}
+                                className="typo-normal"
+                            />
+                            <EngagementType />
+                            <Input
+                                label="Heltid/deltid"
+                                value={ad.properties.extent || ''}
+                                onChange={this.onExtentChange}
+                                className="typo-normal"
+                            />
+                            <Input
+                                label="Antall stillinger"
+                                value={ad.properties.positioncount || ''}
+                                onChange={this.onPositioncountChange}
+                                className="typo-normal"
+                            />
+                            <Input
+                                label="Sektor"
+                                value={ad.properties.sector || ''}
+                                onChange={this.onSectorChange}
+                                className="typo-normal"
+                            />
+                            <Input
+                                label="Arbeidsdager"
+                                value={ad.properties.workday || ''}
+                                onChange={this.onWorkdayChange}
+                                className="typo-normal"
+                            />
+                            <Input
+                                label="Arbeidstid"
+                                value={ad.properties.workhours || ''}
+                                onChange={this.onWorkhoursChange}
+                                className="typo-normal"
+                            />
+                            <Input
+                                label="Arb.tidordning"
+                                value={ad.properties.jobarrangement || ''}
+                                onChange={this.onJobarrangementChange}
+                                className="typo-normal"
+                            />
+                            <Input
+                                label="Oppstart"
+                                value={ad.properties.starttime || ''}
+                                onChange={this.onStarttimeChange}
+                                className="typo-normal"
+                            />
+                        </Ekspanderbartpanel>
+                        <Ekspanderbartpanel className="Edit__panel" tittel="Om arbeidsgiver" tittelProps="undertittel" border="true">
+                            <Input
+                                label="Arbeidsgiver"
+                                value={ad.properties.employer || ''}
+                                onChange={this.onEmployerChange}
+                                className="typo-normal"
+                            />
+                            <Input
+                                label="Arbeidsgiver"
+                                value={ad.properties.employerdescription || ''}
+                                onChange={this.onEmployerDescriptionChange}
+                                className="typo-normal"
+                            />
+                        </Ekspanderbartpanel>
+                        <Ekspanderbartpanel className="Edit__panel" tittel="Om annonsen" tittelProps="undertittel" border="true">
+                            <Input
+                                label="Publisert"
+                                value={ad.published || ''}
+                                onChange={this.onPublishedChange}
+                                className="typo-normal"
+                            />
+                            <Input
+                                label="Sist endret"
+                                value={ad.updated || ''}
+                                onChange={this.onLastUpdatedChange}
+                                className="typo-normal"
+                            />
+                            <Input
+                                label="Hentet fra"
+                                value={ad.medium || ''}
+                                onChange={this.onMediumChange}
+                                className="typo-normal"
+                            />
+                            <Input
+                                label="Løpenr"
+                                value={ad.id || ''}
+                                onChange={this.onIdChange}
+                                className="typo-normal"
+                            />
+                            <Input
+                                label="Referanse"
+                                value={ad.reference || ''}
+                                onChange={this.onReferenceChange}
+                                className="typo-normal"
+                            />
+                            <Input
+                                label="Utløpsdato"
+                                value={ad.expires || ''}
+                                onChange={this.onExpiresChange}
+                                className="typo-normal"
+                            />
+                        </Ekspanderbartpanel>
+                    </div>
                 </Row>
             </div>
         );
