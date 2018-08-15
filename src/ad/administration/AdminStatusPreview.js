a2f50e0e136beccb7cc87cb1f8472b9db62d82fa
Ola Toft-Eriksen <ola.toft-eriksen@nav.no>
2018-08-15 15:37:08 +0200

    Lagre annonse når man setter tilbake til mottatt

--- src/ad/administration/AdminStatusPreview.js
+++ src/ad/administration/AdminStatusPreview.js
@@ -3,7 +3,8 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Normaltekst } from 'nav-frontend-typografi';
 import AdminStatusEnum from './AdminStatusEnum';
-import { SAVE_AD, SET_ADMIN_STATUS } from '../adDataReducer';
+import { SAVE_AD } from '../adReducer';
+import { SET_ADMIN_STATUS } from '../adDataReducer';
 
 class AdminStatusPreview extends React.Component {
     onSetToReceivedClick = (e) => {
