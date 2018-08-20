8abde769eab0edf925cecda5b699cba44bff8012
Tony Gjendahl <tony.gjendahl@nav.no>
2018-08-20 12:53:18 +0200

    Legge til popup ved manglende arbeidsgivertilknytning

--- src/ad/administration/Administration.js
+++ src/ad/administration/Administration.js
@@ -11,22 +11,25 @@ import CommentsEdit from './CommentsEdit';
 import CommentsPreview from './CommentsPreview';
 import AdminStatusPreview from './AdminStatusPreview';
 import AdminStatusEdit from './AdminStatusEdit';
+import { DISCARD_AD_CHANGES, FETCH_NEXT_AD, SAVE_AD } from '../adReducer';
+import { SET_ADMIN_STATUS_AND_GET_NEXT_AD } from "../adDataReducer";
+import AdminStatusNotSavedPopup from "./AdminStatusNotSavedPopup";
+import AdNotSavedPopup from "./AdNotSavedPopup";
+import AdContainsErrorPopup from "./AdContainsErrorPopup";
 import './Administration.less';
-import { FETCH_NEXT_AD } from '../adReducer';
-import ConfirmationPopup from "./ConfirmationPopup";
 
 class Administration extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             isModalOpen: false,
-            next: false
+            isErrorModalOpen: false
         };
     }
 
     onNextClick = () => {
         if (this.props.adminStatus === AdminStatusEnum.PENDING) {
-            this.onOpenPopup(true);
+            this.onOpenPopup();
         } else {
             this.props.getNextAd();
         }
@@ -35,19 +38,41 @@ class Administration extends React.Component {
     onClosePopup = () => {
         this.setState({
             isModalOpen: false,
-            next: false
+            isErrorModalOpen: false
         });
     };
 
-    onOpenPopup = (next = false) => {
+    onOpenPopup = () => {
         this.setState({
-            isModalOpen: true,
-            next
-        })
+            isModalOpen: true
+        });
+    };
+
+    onSaveAdClick = () => {
+        this.props.saveAd();
+    };
+
+    onDiscardClick = () => {
+        this.props.discardAdChanges();
+    };
+
+    onNextWithoutFinishClick = () => {
+        this.props.getNextAd();
+    };
+
+    onNextAndFinishClick = () => {
+        if (this.props.adStatus === AdStatusEnum.ACTIVE && this.props.validation.employer !== undefined) {
+            this.setState({
+                isErrorModalOpen: true,
+                isModalOpen: false
+            });
+        } else {
+            this.props.setAdminStatusAndGetNextAd(AdminStatusEnum.DONE);
+        }
     };
 
     render() {
-        const { adStatus, adminStatus } = this.props;
+        const { adStatus, adminStatus, isEditingAd } = this.props;
         return (
             <div className="Administration">
                 <div className="Administration__flex">
@@ -71,17 +96,25 @@ class Administration extends React.Component {
                     <div className="Administration__flex__bottom">
                         <AdminStatusPreview />
                         <div className="Administration__buttons">
-                            <AdminStatusEdit
-                                closeModal={this.onClosePopup}
-                                openModal={this.onOpenPopup}
-                            />
+                            <AdminStatusEdit />
                             <Knapp className="AdminStatusEdit__button" onClick={this.onNextClick}>
                                 Gå til neste annonse
                             </Knapp>
-                            <ConfirmationPopup
-                                isOpen={this.state.isModalOpen}
-                                onClosePopup={this.onClosePopup}
-                                next={this.state.next}
+                            <AdminStatusNotSavedPopup
+                                isOpen={this.state.isModalOpen && !isEditingAd}
+                                onClose={this.onClosePopup}
+                                onNextAndFinishClick={this.onNextAndFinishClick}
+                                onNextWithoutFinishClick={this.onNextWithoutFinishClick}
+                            />
+                            <AdNotSavedPopup
+                                isOpen={this.state.isModalOpen && isEditingAd}
+                                onClose={this.onClosePopup}
+                                onDiscard={this.onDiscardClick}
+                                onSave={this.onSaveAdClick}
+                            />
+                            <AdContainsErrorPopup
+                                onClose={this.onClosePopup}
+                                isOpen={this.state.isErrorModalOpen}
                             />
                         </div>
                     </div>
@@ -98,16 +131,27 @@ Administration.defaultProps = {
 Administration.propTypes = {
     adStatus: PropTypes.string.isRequired,
     adminStatus: PropTypes.string,
-    getNextAd: PropTypes.func.isRequired
+    getNextAd: PropTypes.func.isRequired,
+    saveAd: PropTypes.func.isRequired,
+    discardAdChanges: PropTypes.func.isRequired,
+    setAdminStatusAndGetNextAd: PropTypes.func.isRequired,
+    validation: PropTypes.shape({
+        employer: PropTypes.string
+    }).isRequired
 };
 
 const mapStateToProps = (state) => ({
     adStatus: state.adData.status,
-    adminStatus: state.adData.administration.status
+    adminStatus: state.adData.administration.status,
+    isEditingAd: state.ad.isEditingAd,
+    validation: state.adValidation.errors
 });
 
 const mapDispatchToProps = (dispatch) => ({
-    getNextAd: () => dispatch({ type: FETCH_NEXT_AD })
+    getNextAd: () => dispatch({ type: FETCH_NEXT_AD }),
+    saveAd: () => dispatch({ type: SAVE_AD }),
+    setAdminStatusAndGetNextAd: (status) => dispatch({ type: SET_ADMIN_STATUS_AND_GET_NEXT_AD, status }),
+    discardAdChanges: () => dispatch({ type: DISCARD_AD_CHANGES })
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Administration);
