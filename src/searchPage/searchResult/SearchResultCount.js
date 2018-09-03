4fa868818b46cbd79dfe0be15a800f6fd0c2c681
Ola Toft-Eriksen <ola.toft-eriksen@nav.no>
2018-09-03 13:41:43 +0200

    [PAM-1334] Endrer tekst til "Søket ga 280 annonser"

--- src/searchPage/searchResult/SearchResultCount.js
+++ src/searchPage/searchResult/SearchResultCount.js
@@ -2,9 +2,8 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { Normaltekst } from 'nav-frontend-typografi';
-import { Fields } from '../searchReducer';
 
-function SearchResultCount({ count, field, isSearching }) {
+function SearchResultCount({ count, isSearching }) {
     if (isSearching) {
         return (
             <Normaltekst className="blokk-s">
@@ -12,41 +11,25 @@ function SearchResultCount({ count, field, isSearching }) {
             </Normaltekst>
         );
     }
-
-    let fieldName = '';
-    if (field === Fields.EMPLOYER_NAME) {
-        fieldName = 'i arbeidsgiver';
-    } else if (field === Fields.TITLE) {
-        fieldName = 'i annonseoverskrift';
-    } else if (field === Fields.ID) {
-        fieldName = 'i stillingsnummer';
-    }
     return (
         <div>
             <Normaltekst className="blokk-s">
-               Søket ga <b>{`${count} treff`}</b> {fieldName}
+                Søket ga <b>{count}</b> annonser
             </Normaltekst>
         </div>
     );
 }
 
-SearchResultCount.defaultProps = {
-    field: undefined
-};
-
 SearchResultCount.propTypes = {
     count: PropTypes.number.isRequired,
-    field: PropTypes.string,
     isSearching: PropTypes.bool.isRequired
 };
 
 const mapStateToProps = (state) => ({
     count: state.search.totalElements,
-    field: state.search.field,
     isSearching: state.search.isSearching
 });
 
-const mapDispatchToProps = () => ({
-});
+const mapDispatchToProps = () => ({});
 
 export default connect(mapStateToProps, mapDispatchToProps)(SearchResultCount);
