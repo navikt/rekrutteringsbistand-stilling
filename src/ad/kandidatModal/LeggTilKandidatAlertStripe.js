123771e21db0a4b66f4e0ffe938ef6262f4fc7d8
Stephen Ramthun <stephen.ramthun@nav.no>
2019-01-07 10:18:12 +0100

    [PAM-2389] Fiks visning av alertstripe når kandidat legges til i kandidatliste

--- src/ad/kandidatModal/LeggTilKandidatAlertStripe.js
+++ src/ad/kandidatModal/LeggTilKandidatAlertStripe.js
@@ -3,12 +3,13 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { AlertStripeSuksess, AlertStripeAdvarselSolid } from 'nav-frontend-alertstriper';
 import { KandidatAlertStripeMode } from './kandidatReducer';
+import './LeggTilKandidatAlertStripe.less';
 
-const LeggTilKandidatAlertStripe = ({ showAlertStripe, alertStripeMode, kandidat }) => {
+const LeggTilKandidatAlertStripe = ({ showAlertStripe, alertStripeMode, kandidat, fodselsnummer }) => {
     if (showAlertStripe && alertStripeMode === KandidatAlertStripeMode.SAVED) {
         return (
             <AlertStripeSuksess solid className="LeggTilKandidatAlertStripe">
-                {`Kandidat ${kandidat.fornavn} ${kandidat.etternavn} er lagt til`}
+                {`${kandidat.fornavn} ${kandidat.etternavn} (${fodselsnummer}) er lagt til i kandidatlisten`}
             </AlertStripeSuksess>
         );
     } else if (showAlertStripe && alertStripeMode === KandidatAlertStripeMode.FAILURE) {
@@ -27,13 +28,15 @@ LeggTilKandidatAlertStripe.propTypes = {
     kandidat: PropTypes.shape({
         fornavn: PropTypes.string,
         etternavn: PropTypes.string
-    })
+    }),
+    fodselsnummer: PropTypes.string
 };
 
 const mapStateToProps = (state) => ({
     showAlertStripe: state.kandidat.showAlertStripe,
     alertStripeMode: state.kandidat.alertStripeMode,
-    kandidat: state.kandidat.kandidat
+    kandidat: state.kandidat.kandidat,
+    fodselsnummer: state.kandidat.fodselsnummer
 });
 
 export default connect(mapStateToProps)(LeggTilKandidatAlertStripe);
