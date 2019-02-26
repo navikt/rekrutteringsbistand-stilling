4ed5d772640899eb71936f4a84b6350323e55a67
Tony Gjendahl <tony.gjendahl@nav.no>
2019-02-26 10:11:11 +0100

    PAM-2360 Oppdatere eieren av stillingen når brukeren trykker på marker som min

--- src/ad/administration/adminStatus/AdminStatusPreview.js
+++ src/ad/administration/adminStatus/AdminStatusPreview.js
@@ -3,30 +3,76 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import { connect } from 'react-redux';
 import './AdminStatusPreview.less';
+import { Flatknapp } from 'nav-frontend-knapper';
+import { SET_REPORTEE, SET_NAV_IDENT } from '../../adDataReducer';
+import { SAVE_AD } from '../../adReducer';
 
-const AdminStatusPreview = ({ reportee, navIdent }) => (
-    <div className="AdminStatusPreview">
-        <Normaltekst>
-            <b>Registrert av: </b>
-            {reportee || ''}
-            {navIdent ? ` (${navIdent})` : ''}
-        </Normaltekst>
-    </div>
-);
+class AdminStatusPreview extends React.Component {
+    onMarkAsMineClick = () => {
+        const {
+            reportee,
+            setReportee,
+            setNavIdent,
+            saveAd
+        } = this.props;
+        setReportee(reportee.displayName);
+        setNavIdent(reportee.navIdent);
+        saveAd();
+    }
+
+    render() {
+        const { administration, reportee } = this.props;
+        return (
+            <div className="AdminStatusPreview">
+                <div>
+                    <Normaltekst>
+                        <b>Registrert av: </b>
+                        {administration.reportee || ''}
+                        {administration.navIdent ? ` (${administration.navIdent})` : ''}
+                    </Normaltekst>
+                </div>
+                {reportee && reportee.navIdent !== administration.navIdent && (
+                    <div>
+                        <Flatknapp
+                            mini
+                            onClick={this.onMarkAsMineClick}
+                        >
+                            Marker som min
+                        </Flatknapp>
+                    </div>
+                )}
+            </div>
+        );
+    }
+}
 
 AdminStatusPreview.defaultProps = {
-    reportee: undefined,
-    navIdent: undefined
+    administration: {}
 };
 
 AdminStatusPreview.propTypes = {
-    reportee: PropTypes.string,
-    navIdent: PropTypes.string
+    administration: PropTypes.shape({
+        reportee: PropTypes.string,
+        navIdent: PropTypes.string
+    }),
+    reportee: PropTypes.shape({
+        navIdent: PropTypes.string,
+        displayName: PropTypes.string
+    }).isRequired,
+    setReportee: PropTypes.func.isRequired,
+    setNavIdent: PropTypes.func.isRequired,
+    saveAd: PropTypes.func.isRequired
 };
 
-const mapStateToProps = (state) => ({
-    reportee: state.adData.administration.reportee,
-    navIdent: state.adData.administration.navIdent
+const mapDispatchToProps = (dispatch) => ({
+    setReportee: (reportee) => dispatch({ type: SET_REPORTEE, reportee }),
+    setNavIdent: (navIdent) => dispatch({ type: SET_NAV_IDENT, navIdent }),
+    saveAd: () => dispatch({ type: SAVE_AD })
 });
 
-export default connect(mapStateToProps)(AdminStatusPreview);
+const mapStateToProps = (state) => ({
+    administration: state.adData.administration,
+    reportee: state.reportee.data
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(AdminStatusPreview);
