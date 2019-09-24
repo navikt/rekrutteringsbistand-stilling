c2492ad01311ca7e07d4d1ebded6ebbb4c55bdc8
joar <joar.aurdal@gmail.com>
2019-09-24 12:58:31 +0200

    tag-643 lange lenker i kommentar

--- src/ad/preview/administration/comment/Comment.js
+++ src/ad/preview/administration/comment/Comment.js
@@ -15,7 +15,7 @@ class Comment extends React.Component {
 
         const config = [{
             regex: /(http|https):\/\/(\S+)\.([a-z]{2,}?)(.*?)( |\,|$|\.)/gim,
-            fn: (key, result) => <span key={key}>
+            fn: (key, result) => <span key={key} className="Comment__text__link">
                                      <a target="_blank" href={`${result[1]}://${result[2]}.${result[3]}${result[4]}`}>{result[2]}.{result[3]}{result[4]}</a>{result[5]}
                                  </span>
         }];
