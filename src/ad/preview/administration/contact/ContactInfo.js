f947a595db76f7baa240e12878ca6b093ebaf9ae
joar <joar.aurdal@gmail.com>
2019-11-05 10:31:55 +0100

    pam-1895 legger inn identvisning for veileder

--- src/ad/preview/administration/contact/ContactInfo.js
+++ src/ad/preview/administration/contact/ContactInfo.js
@@ -1,26 +1,24 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import { Element, Normaltekst } from 'nav-frontend-typografi';
 import './ContactInfo.less';
 
 class ContactInfo extends React.Component {
 
   render() {
-    const { stilling } = this.props;
-    const showContactHelp = (stilling && stilling.source === 'DIR');
+    const { stilling, recruitment } = this.props;
+    const isDir = (stilling && stilling.source === 'DIR');
+    const hasRecruitment = recruitment && recruitment.overfoertTil;
     const { reportee, navIdent } = stilling.administration;
     const contact = stilling.contactList && stilling.contactList.length ? stilling.contactList[0] : undefined;
 
-    return showContactHelp ?
+    return isDir ?
     <div className="ContactInfo__preview">
         <Element>Spørsmål om stillingen?</Element>
         <Normaltekst>
           Kontaktperson hos NAV: {reportee} {navIdent ? ` (${navIdent})` : ''}
         </Normaltekst>
-        {stilling.props && stilling.pr.navidentrekruttering && 
-        <Normaltekst>
-          Kontaktperson rekruttering: {stilling.props.navidentrekruttering}
-        </Normaltekst>}
         {contact && contact.email && 
         <Normaltekst>
           E-post: {contact.email}
@@ -31,19 +29,37 @@ class ContactInfo extends React.Component {
         </Normaltekst>}
       </div> : 
       <div className="ContactInfo__preview">
-        {stilling.properties &&
-                            stilling.properties.navidentrekruttering && (
-                                <Normaltekst>
-                                    Kontaktperson rekruttering:{' '}
-                                    {stilling.properties.navidentrekruttering}
-                                </Normaltekst>
-                            )}
+        {hasRecruitment && (
+          <Normaltekst>
+            Kontaktperson hos NAV: todo {recruitment.overfoertTil ? ` (${recruitment.overfoertTil})` : ''}
+          </Normaltekst>
+        )}
       </div>
   }
 }
 
+ContactInfo.defaultProps = {
+  stilling: undefined,
+  recruitment: undefined
+}
+
+ContactInfo.propTypes = {
+  stilling: PropTypes.shape({
+    source: PropTypes.string,
+    administration: PropTypes.shape({
+      reportee: PropTypes.string,
+      navIdent: PropTypes.string
+    })
+  }),
+  recruitment: PropTypes.shape({
+    stillingUuid: PropTypes.string,
+    overfoertTil: PropTypes.string
+  })
+}
+
 const mapStateToProps = (state) => ({
-  stilling: state.adData
+  stilling: state.adData,
+  recruitment: state.recruitmentData
 });
 
 export default connect(mapStateToProps)(ContactInfo); 
