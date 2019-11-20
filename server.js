96b2fa3c9cfad9b0261d0e30ac3fc319f651cad8
joar <joar.aurdal@gmail.com>
2019-11-20 16:09:15 +0100

    fikset config av api

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ server.engine('html', mustacheExpress());
 
 
 const writeEnvironmentVariablesToFile = () => {
-    const fileContent = `window.__w__="${process.env.REKRUTTERINGSBISTAND_API_URL}";\n`
+    const fileContent = `window.__PAM_AD_API__="${process.env.REKRUTTERINGSBISTAND_API_URL}";\n`
         + `window.__REKRUTTERING_API__="${process.env.REKRUTTERING_API_URL}";\n`
         + `window.__PAM_SEARCH_API__="${process.env.REKRUTTERINGSBISTAND_API_SEARCH_URL}";\n`
         + `window.__PAM_CONTEXT_PATH__="";\n`
