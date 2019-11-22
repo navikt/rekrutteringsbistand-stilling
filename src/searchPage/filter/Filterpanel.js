12966c9fe52e07629578a6855135a96c6c845fbc
Kjetil Svalestuen <kjetil.svalestuen@nav.no>
2019-11-22 14:50:50 +0100

    Refaktor Filterpanel for accessibility

--- src/searchPage/filter/Filterpanel.js
+++ src/searchPage/filter/Filterpanel.js
@@ -2,19 +2,25 @@ import React from 'react';
 import { EkspanderbartpanelBase } from 'nav-frontend-ekspanderbartpanel';
 import './Filterpanel.less';
 
-const Filterpanel = ({ label, htmlFor, children }) => (
-    <EkspanderbartpanelBase
-        className="Filterpanel blokk-s"
-        border
-        apen
-        heading={
-            <label className="typo-element skjemaelement__label" htmlFor={htmlFor}>
-                {label}
-            </label>
-        }
-    >
-        {children}
-    </EkspanderbartpanelBase>
-);
+const Filterpanel = ({ label, children }) => {
+    const accessibilityHeader = `${label}_label`;
+
+    return (
+        <EkspanderbartpanelBase
+            className="Filterpanel blokk-s"
+            border
+            apen
+            heading={
+                <label id={accessibilityHeader} className="typo-element skjemaelement__label">
+                    {label}
+                </label>
+            }
+        >
+            <div role="group" aria-labelledby={accessibilityHeader}>
+                {children}
+            </div>
+        </EkspanderbartpanelBase>
+    );
+};
 
 export default Filterpanel;
