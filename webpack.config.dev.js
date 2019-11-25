b8f82110a7d05cc842b750163edb76e14b4d9f53
Kjetil Svalestuen <kjetil.svalestuen@nav.no>
2019-11-25 08:54:32 +0100

    Formater diverse filer

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -6,30 +6,33 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = merge(common, {
     devtool: 'inline-source-map',
     devServer: {
-        historyApiFallback: true
+        historyApiFallback: true,
     },
     module: {
-        loaders: [{
-            test: /index\.html$/,
-            loader: 'mustache-loader',
-            options: {
-                render: {
-                    isProduction: false,
-                }
-            }
-        }]
+        loaders: [
+            {
+                test: /index\.html$/,
+                loader: 'mustache-loader',
+                options: {
+                    render: {
+                        isProduction: false,
+                    },
+                },
+            },
+        ],
     },
     plugins: [
         new webpack.DefinePlugin({
             __PAM_CONTEXT_PATH__: "''",
-            __PAM_AD_API__: "'http://localhost:9501/rekrutteringsbistand-api/rekrutteringsbistand/api/v1/'",
+            __PAM_AD_API__:
+                "'http://localhost:9501/rekrutteringsbistand-api/rekrutteringsbistand/api/v1/'",
             __REKRUTTERING_API__: "'http://localhost:9501/rekrutteringsbistand-api/rekruttering'",
             __PAM_SEARCH_API__: "'http://localhost:9501/rekrutteringsbistand-api/search-api/'",
             __PAM_LOGIN_URL__: "'http://localhost:9501/rekrutteringsbistand-api/local/cookie-isso'",
-            __PAM_KANDIDATLISTE_API_URL__: "'http://localhost:8766/kandidater/rest/veileder'"
+            __PAM_KANDIDATLISTE_API_URL__: "'http://localhost:8766/kandidater/rest/veileder'",
         }),
         new HtmlWebpackPlugin({
-            template: './src/index.html'
-        })
-    ]
+            template: './src/index.html',
+        }),
+    ],
 });
