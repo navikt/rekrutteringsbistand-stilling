6d518bd4e08db2d393f3b78fcdd437531f7d9e93
Kjetil Svalestuen <kjetil.svalestuen@nav.no>
2020-03-11 09:42:25 +0100

    Fjern utkommentert kode

--- src/navigeringsmeny/Navigeringsmeny.tsx
+++ src/navigeringsmeny/Navigeringsmeny.tsx
@@ -1,46 +1,16 @@
 import React, { FunctionComponent } from 'react';
 import { connect } from 'react-redux';
-// import { FETCH_REPORTEE } from '../reportee/reporteeReducer';
-import { SHOW_HAS_CHANGES_MODAL } from '../ad/adReducer';
-import 'pam-frontend-header/dist/style.css';
-import NyttIRekrutteringsbistand from '@navikt/nytt-i-rekrutteringsbistand';
-import '../../node_modules/@navikt/nytt-i-rekrutteringsbistand/lib/nytt.css';
-import './Navigeringsmeny.less';
-import Tab, { TabConfig } from './Tab';
-import { useLocation, Link } from 'react-router-dom';
 import { Location } from 'history';
+import { useLocation } from 'react-router-dom';
+import NyttIRekrutteringsbistand from '@navikt/nytt-i-rekrutteringsbistand';
+
+import { SHOW_HAS_CHANGES_MODAL } from '../ad/adReducer';
+import Tab, { TabConfig } from './Tab';
 import Hus from './Hus';
-/*
-class Navigeringsmeny extends React.Component {
-    componentDidMount() {
-        const { displayName, fetchDisplayName, isFetchingDisplayName }: any = this.props;
 
-        if (!displayName && !isFetchingDisplayName) {
-            fetchDisplayName();
-        }
-    }
-
-    render() {
-        const { tabId, displayName, showHasChangesModal, hasChanges, visNyheter }: any = this.props;
-        return (
-            <div className="navigeringsmeny">
-                <VeilederHeaderMeny
-                    activeTabID={tabId}
-                    innloggetBruker={displayName}
-                    validerNavigasjon={{
-                        redirectTillates: () => !hasChanges,
-                        redirectForhindretCallback: url => showHasChangesModal(url),
-                    }}
-                />
-                {visNyheter && (
-                    <div className="navigeringsmeny__nyheter">
-                        {/*<NyttIRekrutteringsbistand orientering="under-hoyre" />
-                    </div>
-                )}
-            </div>
-        );
-    }
-}*/
+import '../../node_modules/@navikt/nytt-i-rekrutteringsbistand/lib/nytt.css';
+import 'pam-frontend-header/dist/style.css';
+import './Navigeringsmeny.less';
 
 const tabs: TabConfig[] = [
     {
