0db3b9c836579be8a0f9da45df511044e9415a9d
André Mossige <andre.mossige@nav.no>
2020-03-11 12:47:49 +0100

    Vis modal hvis man har ulagret arbeid (#127)

--- src/navigeringsmeny/Navigeringsmeny.tsx
+++ src/navigeringsmeny/Navigeringsmeny.tsx
@@ -1,5 +1,4 @@
 import React, { FunctionComponent } from 'react';
-import { connect } from 'react-redux';
 import { Location } from 'history';
 import { useLocation } from 'react-router-dom';
 import NyttIRekrutteringsbistand from '@navikt/nytt-i-rekrutteringsbistand';
@@ -7,6 +6,7 @@ import NyttIRekrutteringsbistand from '@navikt/nytt-i-rekrutteringsbistand';
 import { SHOW_HAS_CHANGES_MODAL } from '../ad/adReducer';
 import Tab, { TabConfig } from './Tab';
 import Hus from './Hus';
+import { useSelector, useDispatch } from 'react-redux';
 
 import '../../node_modules/@navikt/nytt-i-rekrutteringsbistand/lib/nytt.css';
 import 'pam-frontend-header/dist/style.css';
@@ -35,20 +35,32 @@ const tabs: TabConfig[] = [
     },
 ];
 
-type Props = {
-    showHasChangesModal: () => void;
-    hasChanges: boolean;
-};
-
-const Navigeringsmeny: FunctionComponent<Props> = props => {
+const Navigeringsmeny: FunctionComponent = () => {
     const { pathname }: Location = useLocation();
+    const { hasChanges } = useSelector(state => state.ad);
+    const dispatch = useDispatch();
+
+    const visForlatSidenModal = (leaveUrl: String) =>
+        dispatch({ type: SHOW_HAS_CHANGES_MODAL, leaveUrl });
+
+    const onClickTab = (href: string) => (event: React.MouseEvent<HTMLElement>) => {
+        if (hasChanges) {
+            event.preventDefault();
+            visForlatSidenModal(href);
+        }
+    };
 
     return (
         <div className="navigeringsmeny">
             <nav className="navigeringsmeny__tabs">
-                <Hus href="/" erAktiv={pathname === '/'} />
+                <Hus href="/" erAktiv={pathname === '/'} onClick={onClickTab('/')} />
                 {tabs.map(tab => (
-                    <Tab key={tab.href} config={tab} erAktiv={pathname === tab.href} />
+                    <Tab
+                        key={tab.href}
+                        config={tab}
+                        erAktiv={pathname.startsWith(tab.href)}
+                        onClick={onClickTab(tab.href)}
+                    />
                 ))}
             </nav>
             <div className="navigeringsmeny__nyheter">
@@ -58,12 +70,4 @@ const Navigeringsmeny: FunctionComponent<Props> = props => {
     );
 };
 
-const mapStateToProps = (state: any) => ({
-    hasChanges: state.ad.hasChanges,
-});
-
-const mapDispatchToProps = (dispatch: any) => ({
-    showHasChangesModal: (leaveUrl: any) => dispatch({ type: SHOW_HAS_CHANGES_MODAL, leaveUrl }),
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(Navigeringsmeny);
+export default Navigeringsmeny;
