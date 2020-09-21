ed7e4e9b41070eb959987e412b54b78bfb785e4f
Kjetil Svalestuen <kjetil.svalestuen@nav.no>
2020-09-21 13:17:24 +0200

    Logg sidevisninger med Amplitude

---
+++ src/useLoggNavigering.ts
@@ -0,0 +1,33 @@
+import { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import { sendEvent } from './amplitude';
+
+const stierSomSkalLogges = ['/', '/stillinger', '/minestillinger'];
+const begynnelsenAvStierSomSkalLogges = ['/stilling/'];
+
+const useLoggNavigering = () => {
+    const history = useHistory();
+    const pathname = history.location.pathname;
+
+    const loggNavigering = (side: string) => {
+        sendEvent('app', 'sidevisning', {
+            side,
+        });
+    };
+
+    useEffect(() => {
+        if (stierSomSkalLogges.includes(pathname)) {
+            loggNavigering(pathname);
+        } else {
+            const begynnelsenAvSti = begynnelsenAvStierSomSkalLogges.find((sti) =>
+                pathname.startsWith(sti)
+            );
+
+            if (begynnelsenAvSti) {
+                loggNavigering(begynnelsenAvSti);
+            }
+        }
+    }, [pathname]);
+};
+
+export default useLoggNavigering;
