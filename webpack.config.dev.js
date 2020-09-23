c8c55b0e73faba1803f1ea782acd699c8d930386
Kjetil Svalestuen <kjesvale@users.noreply.github.com>
2020-09-23 10:24:02 +0200

    Oppgrader og fiks bygging (#175)
    * Oppgrader webpack + less, bruk nye options for webpack i dev/prod
    
    * Fjern gammel uglifyjs-plugin, fjern ubrukt webpack-config for E2E-tester
    
    * Splitt node_modules til egen output-fil med ny webpack
    
    * Deploy til dev
    
    * Bruk contenthash for caching av node_modules

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -3,9 +3,9 @@ const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = env =>
+module.exports = (env) =>
     merge(common, {
-        devtool: 'inline-source-map',
+        mode: 'development',
         devServer: {
             historyApiFallback: true,
         },
