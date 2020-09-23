fa2b199ed4539971d294d3a43ba234e0ac6ee5a3
Kjetil Svalestuen <kjetil.svalestuen@nav.no>
2020-09-23 08:29:12 +0200

    Oppgrader webpack + less, bruk nye options for webpack i dev/prod

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
 const webpack = require('webpack');
 const merge = require('webpack-merge');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const common = require('./webpack.common.js');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = () => {
     return merge(common, {
+        mode: 'production',
         devtool: 'source-map',
         module: {
             rules: [
@@ -22,15 +22,11 @@ module.exports = () => {
         },
         plugins: [
             new webpack.DefinePlugin({
-                'process.env.NODE_ENV': JSON.stringify('production'),
                 MOCK_APP: false,
             }),
             new HtmlWebpackPlugin({
                 template: './src/index.html',
             }),
-            /* Optimize bundle load time */
-            new webpack.optimize.ModuleConcatenationPlugin(),
-            new UglifyJSPlugin({ sourceMap: true }),
         ],
     });
 };
