6b89c289a7df0dd377e118273e8c47c1524d4310
Kjetil Svalestuen <kjetil.svalestuen@nav.no>
2020-10-02 14:33:41 +0200

    Bruk ny struktur i visning av inkluderingsmuligheter

--- src/ad/preview/vis-inkluderingsmuligheter-intern-stilling/Inkluderingsmuligheter.tsx
+++ src/ad/preview/vis-inkluderingsmuligheter-intern-stilling/Inkluderingsmuligheter.tsx
@@ -1,83 +1,69 @@
 import React, { FunctionComponent } from 'react';
 import { Element, Normaltekst } from 'nav-frontend-typografi';
-import { GruppeMedTags, Tag } from '../../tags';
+import {
+    hentSubtagsForMulighetForDirektemeldtStilling,
+    InkluderingsmulighetForDirektemeldtStilling,
+    Tag,
+} from '../../tags/hierarkiAvTags';
+import { visningsnavnForRegistrering } from '../../tags/visningsnavnForTags';
 
 interface Props {
     registrerteTags: Tag[];
-    alleRelevanteGrupperMedTags: GruppeMedTags[];
 }
 
-const Inkluderingsmuligheter: FunctionComponent<Props> = ({
-    registrerteTags,
-    alleRelevanteGrupperMedTags,
-}) => {
-    const registrerteInkuderingstags: GruppeMedTags[] = alleRelevanteGrupperMedTags
-        .filter(gruppeMedTagsErRegistrertOgHarSubtags(registrerteTags))
-        .map(fjernSubtagsSomIkkeErRegistrert(registrerteTags));
+const Inkluderingsmuligheter: FunctionComponent<Props> = ({ registrerteTags }) => {
+    const tagsInnenTilrettelegging = hentSubtagsForMulighetForDirektemeldtStilling(
+        InkluderingsmulighetForDirektemeldtStilling.Tilrettelegging
+    ).filter((tag) => registrerteTags.includes(tag));
+
+    const tagsInnenPrioriterteMålgrupper = hentSubtagsForMulighetForDirektemeldtStilling(
+        InkluderingsmulighetForDirektemeldtStilling.PrioriterteMålgrupper
+    ).filter((tag) => registrerteTags.includes(tag));
+
+    const tagsInnenTiltakEllerVirkemidler = hentSubtagsForMulighetForDirektemeldtStilling(
+        InkluderingsmulighetForDirektemeldtStilling.TiltakEllerVirkemiddel
+    ).filter((tag) => registrerteTags.includes(tag));
 
     return (
         <>
-            {registrerteInkuderingstags.map((gruppeMedTags) => (
-                <div key={gruppeMedTags.tag} className="blokk-s">
-                    <Element>{gruppeMedTags.navn}</Element>
-                    {gruppeMedTags.harSubtags && (
-                        <ul className="vis-inkluderingsmuligheter-intern-stilling__tagliste">
-                            {gruppeMedTags.subtags.map((subtag) => (
-                                <Normaltekst tag="li" key={subtag.tag}>
-                                    {visningsnavnForSubtags[subtag.tag]}
-                                </Normaltekst>
-                            ))}
-                        </ul>
-                    )}
+            {tagsInnenTilrettelegging.length > 0 && (
+                <div className="blokk-s">
+                    <Element>Arbeidsgiver kan tilrettelegge for</Element>
+                    <ul className="vis-inkluderingsmuligheter-intern-stilling__tagliste">
+                        {tagsInnenTilrettelegging.map((tag) => (
+                            <Normaltekst tag="li" key={tag}>
+                                {visningsnavnForRegistrering[tag]}
+                            </Normaltekst>
+                        ))}
+                    </ul>
                 </div>
-            ))}
+            )}
+            {tagsInnenPrioriterteMålgrupper.length > 0 && (
+                <div className="blokk-s">
+                    <Element>Arbeidsgiveren er åpen for kandidater som </Element>
+                    <ul className="vis-inkluderingsmuligheter-intern-stilling__tagliste">
+                        {tagsInnenPrioriterteMålgrupper.map((tag) => (
+                            <Normaltekst tag="li" key={tag}>
+                                {visningsnavnForRegistrering[tag]}
+                            </Normaltekst>
+                        ))}
+                    </ul>
+                </div>
+            )}
+            {tagsInnenTiltakEllerVirkemidler.length > 0 && (
+                <div className="blokk-s">
+                    <Element>Arbeidsgiveren er åpen for kandidater som </Element>
+                    <ul className="vis-inkluderingsmuligheter-intern-stilling__tagliste">
+                        {tagsInnenTiltakEllerVirkemidler.map((tag) => (
+                            <Normaltekst tag="li" key={tag}>
+                                {visningsnavnForRegistrering[tag]}
+                            </Normaltekst>
+                        ))}
+                    </ul>
+                </div>
+            )}
         </>
     );
-};
-
-const gruppeMedTagsErRegistrertOgHarSubtags = (registrerteTags: Tag[]) => (
-    gruppeMedTags: GruppeMedTags
-) => registrerteTags.includes(gruppeMedTags.tag) && gruppeMedTags.harSubtags;
-
-const fjernSubtagsSomIkkeErRegistrert = (registrerteTags: Tag[]) => (
-    gruppeMedTags: GruppeMedTags
-): GruppeMedTags => {
-    if (gruppeMedTags.harSubtags) {
-        const subtagsRegistrertPåStillingen = gruppeMedTags.subtags.filter((subtag) =>
-            registrerteTags.includes(subtag.tag)
-        );
-
-        const inkluderingsmulighetTittel = inkluderingsmulighetTilVisningsnavn(
-            gruppeMedTags.tag,
-            subtagsRegistrertPåStillingen.length > 0
-        );
-
-        return {
-            ...gruppeMedTags,
-            navn: inkluderingsmulighetTittel,
-            subtags: subtagsRegistrertPåStillingen,
-        };
-    } else {
-        return gruppeMedTags;
-    }
-};
-
-const visningsnavnForSubtags: Partial<Record<Tag, string>> = {
-    [Tag.InkluderingArbeidstid]: 'arbeidstid',
-    [Tag.InkluderingFysisk]: 'fysisk tilrettelegging',
-    [Tag.InkluderingArbeidshverdagen]: 'arbeidshverdagen',
-    [Tag.InkluderingGrunnleggende]: 'utfordringer med norsk',
-};
-
-const inkluderingsmulighetTilVisningsnavn = (inkluderingsmulighet: Tag, harSubtags: boolean) => {
-    switch (inkluderingsmulighet) {
-        case Tag.Inkludering:
-            return harSubtags
-                ? 'Arbeidsgiveren kan gjøre tilpasninger rundt:'
-                : 'Arbeidsgiveren kan tilrettelegge';
-        default:
-            return 'Ukjent inkluderingsmulighet';
-    }
 };
 
 export default Inkluderingsmuligheter;
