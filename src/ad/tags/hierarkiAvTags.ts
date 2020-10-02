d40045a9e8bdf22615aefb7e017b73ce63e29c18
Kjetil Svalestuen <kjetil.svalestuen@nav.no>
2020-10-02 10:50:57 +0200

    Separar hierarki av tags fra visningslogikk

---
+++ src/ad/tags/hierarkiAvTags.ts
@@ -0,0 +1,83 @@
+export enum Tag {
+    Tilrettelegging = 'INKLUDERING',
+    TilretteleggingArbeidstid = 'INKLUDERING__ARBEIDSTID',
+    TilretteleggingFysisk = 'INKLUDERING__FYSISK',
+    TilretteleggingArbeidshverdagen = 'INKLUDERING__ARBEIDSMILJØ',
+    TilretteleggingGrunnleggende = 'INKLUDERING__GRUNNLEGGENDE',
+
+    TiltakLønnstilskudd = 'TILTAK__LØNNSTILSKUDD',
+    TiltakMentortilskudd = 'TILTAK__MENTORTILSKUDD',
+
+    Målgruppe = 'MÅLGRUPPE',
+    MålgruppeErUngeUnder30 = 'MÅLGRUPPE__ER_UNGE_UNDER_30',
+    MålgruppeErSeniorerOver45 = 'MÅLGRUPPE__ER_SENIORER_OVER_45',
+    MålgruppeKommerFraLandUtenforEØS = 'MÅLGRUPPE__KOMMER_FRA_LAND_UTENFOR_EØS',
+    MålgruppeHullICVen = 'MÅLGRUPPE__HULL_I_CV_EN',
+    MålgruppeLiteEllerIngenUtdanning = 'MÅLGRUPPE__LITE_ELLER_INGEN_UTDANNING',
+    MålgruppeLiteEllerIngenArbeidserfaring = 'MÅLGRUPPE__LITE_ELLER_INGEN_ARBEIDSERFARING',
+
+    StatligInkluderingsdugnad = 'STATLIG_INKLUDERINGSDUGNAD',
+}
+
+enum Gruppenavn {
+    Tilrettelegging,
+    TiltakEllerVirkemiddel,
+    PrioriterteMålgrupper,
+    StatligInkluderingsdugnad,
+}
+
+type GruppeMedTags = {
+    hovedtag?: Tag;
+    subtags?: Tag[];
+};
+
+export type HierarkiAvTags = Partial<Record<Gruppenavn, GruppeMedTags>>;
+
+export const hierarkiAvTagsForDirektemeldteStillinger: HierarkiAvTags = {
+    [Gruppenavn.Tilrettelegging]: {
+        hovedtag: Tag.Tilrettelegging,
+        subtags: [
+            Tag.TilretteleggingArbeidstid,
+            Tag.TilretteleggingArbeidshverdagen,
+            Tag.TilretteleggingFysisk,
+            Tag.TilretteleggingGrunnleggende,
+        ],
+    },
+
+    [Gruppenavn.TiltakEllerVirkemiddel]: {
+        subtags: [Tag.TiltakLønnstilskudd, Tag.TiltakMentortilskudd],
+    },
+
+    [Gruppenavn.PrioriterteMålgrupper]: {
+        hovedtag: Tag.Målgruppe,
+        subtags: [
+            Tag.MålgruppeErUngeUnder30,
+            Tag.MålgruppeErSeniorerOver45,
+            Tag.MålgruppeKommerFraLandUtenforEØS,
+            Tag.MålgruppeHullICVen,
+            Tag.MålgruppeLiteEllerIngenUtdanning,
+            Tag.MålgruppeLiteEllerIngenArbeidserfaring,
+        ],
+    },
+};
+
+export const hierarkiAvTagsForEksterneStillinger: HierarkiAvTags = {
+    [Gruppenavn.Tilrettelegging]: {
+        hovedtag: Tag.Tilrettelegging,
+        subtags: [
+            Tag.TilretteleggingArbeidstid,
+            Tag.TilretteleggingArbeidshverdagen,
+            Tag.TilretteleggingFysisk,
+            Tag.TilretteleggingGrunnleggende,
+        ],
+    },
+
+    [Gruppenavn.StatligInkluderingsdugnad]: {
+        hovedtag: Tag.StatligInkluderingsdugnad,
+    },
+};
+
+export const hentHierarkiAvTags = (stillingErDirektemeldt: boolean = false): HierarkiAvTags =>
+    stillingErDirektemeldt
+        ? hierarkiAvTagsForDirektemeldteStillinger
+        : hierarkiAvTagsForEksterneStillinger;
