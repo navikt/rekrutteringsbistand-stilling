2c3aa2660f3c4dc9ff60c628e6bc6dd71ef43993
Kjetil Svalestuen <kjetil.svalestuen@nav.no>
2020-10-06 09:28:39 +0200

    Legg til jest og noen enhetstester for inkluderingstags

---
+++ src/ad/tags/utils.test.ts
@@ -0,0 +1,46 @@
+import { Tag } from './hierarkiAvTags';
+import { leggTilTagUnderRegistrering, fjernTagUnderRegistrering } from './utils';
+
+const utenTilretteleggingFysisk = [
+    Tag.Målgruppe,
+    Tag.MålgruppeErUngeUnder30,
+    Tag.Tilrettelegging,
+    Tag.TilretteleggingArbeidshverdagen,
+];
+
+const medTilretteleggingFysisk = [...utenTilretteleggingFysisk, Tag.TilretteleggingFysisk];
+
+describe('Registrere inkluderingsmuligheter på en stilling', () => {
+    test('Å legge til en subtag', () => {
+        expect(
+            leggTilTagUnderRegistrering(utenTilretteleggingFysisk, Tag.TilretteleggingFysisk)
+        ).toEqual(expect.arrayContaining(medTilretteleggingFysisk));
+    });
+
+    test('Å legge til en subtag skal også legge til dens hovedtag', () => {
+        expect(leggTilTagUnderRegistrering([], Tag.TilretteleggingFysisk)).toEqual(
+            expect.arrayContaining([Tag.Tilrettelegging, Tag.TilretteleggingFysisk])
+        );
+    });
+
+    test('Å legge til en subtag skal ikke legge til dens hovedtag hvis den er registrert fra før', () => {
+        expect(leggTilTagUnderRegistrering([Tag.Målgruppe], Tag.MålgruppeHullICVen)).toEqual(
+            expect.arrayContaining([Tag.Målgruppe, Tag.MålgruppeHullICVen])
+        );
+    });
+
+    test('Fjerning av en subtag', () => {
+        expect(
+            fjernTagUnderRegistrering(medTilretteleggingFysisk, Tag.TilretteleggingFysisk)
+        ).toEqual(expect.arrayContaining(utenTilretteleggingFysisk));
+    });
+
+    test('Fjerning av siste subtag innen en inkluderingsmulighet skal også fjerne dens hovedtag', () => {
+        expect(
+            fjernTagUnderRegistrering(
+                [Tag.Målgruppe, Tag.MålgruppeErUngeUnder30],
+                Tag.MålgruppeErUngeUnder30
+            )
+        ).toEqual(expect.arrayContaining([]));
+    });
+});
