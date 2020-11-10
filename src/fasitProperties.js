9a1ffed8b1904a06f97e1d1b2f0fee97514a20e2
Joar Aurdal <joar.aurdal@gmail.com>
2020-11-10 11:02:17 +0100

    Refactor env variabler (#200)
    * Refactor env variabler
    
    * deploy test

--- src/fasitProperties.js
+++ src/fasitProperties.js
@@ -1,10 +1,12 @@
 /* eslint-disable */
 
-export const AD_API = __STILLING_PAM_AD_API__;
-export const LOGIN_URL = __STILLING_PAM_LOGIN_URL__;
-export const SEARCH_API = __STILLING_PAM_SEARCH_API__;
-export const KANDIDATLISTE_API = __STILLING_PAM_KANDIDATLISTE_API_URL__;
+export const REKRUTTERINGSBISTAND_BASE_URL = window.__STILLING_REKRUTTERINGSBISTAND_BASE_URL__;
+export const AD_API = `${REKRUTTERINGSBISTAND_BASE_URL}/rekrutteringsbistand/api/v1/`;
+export const SEARCH_API = `${REKRUTTERINGSBISTAND_BASE_URL}/search-api/`;
+export const REKRUTTERING_API = `${REKRUTTERINGSBISTAND_BASE_URL}/rekruttering/`;
+
+export const KANDIDATLISTE_API = '/kandidater/rest/veileder';
 export const KANDIDATSOK_API = `${KANDIDATLISTE_API}/kandidatsok`;
-export const REKRUTTERING_API = __STILLING_REKRUTTERING_API__;
-export const REKRUTTERINGSBISTAND_BASE_URL = __STILLING_REKRUTTERINGSBISTAND_BASE_URL__;
-export const VIS_STILLING_URL = __STILLING_VIS_STILLING_URL__;
+
+export const VIS_STILLING_URL = window.__STILLING_VIS_STILLING_URL__;
+export const LOGIN_URL = window.__STILLING_PAM_LOGIN_URL__;
