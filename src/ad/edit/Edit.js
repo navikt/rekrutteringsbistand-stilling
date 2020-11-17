53411679ff665036741269886efd0b1d6c1e67c7
André Mossige <mossige.a@gmail.com>
2020-11-17 08:58:07 +0100

    Fjern gammel innlogging som ikke gjelder lengre

--- src/ad/edit/Edit.js
+++ src/ad/edit/Edit.js
@@ -4,7 +4,6 @@ import { Input } from 'nav-frontend-skjema';
 import Ekspanderbartpanel from 'nav-frontend-ekspanderbartpanel';
 import { Column } from 'nav-frontend-grid';
 import { connect } from 'react-redux';
-import { SAVE_AD } from '../adReducer';
 import { RESET_VALIDATION_ERROR } from '../adValidationReducer';
 import './Edit.less';
 import PracticalInformation from './practicalInformation/PracticalInformation';
@@ -14,12 +13,6 @@ import ContactPerson from './contactPerson/ContactPerson';
 import Application from './application/Application';
 import Location from './location/Location';
 import { formatISOString } from '../../utils';
-import { loginWithRedirectToCurrentLocation } from '../../login';
-import TokenExpirationChecker, {
-    TOKEN_EXPIRES_SOON,
-    TOKEN_HAS_EXPIRED,
-} from './session/TokenExpirationChecker';
-import SessionExpirationModal from './session/SessionExpirationModal';
 import EditHeader from './header/EditHeader';
 import { hentAnnonselenke, stillingErPublisert } from '../adUtils';
 import CandidateActions from '../candidateActions/CandidateActions';
@@ -32,47 +25,14 @@ import AlertStripe from 'nav-frontend-alertstriper';
 class Edit extends React.Component {
     constructor(props) {
         super(props);
-        this.tokenExpirationChecker = new TokenExpirationChecker();
-        this.tokenExpirationChecker.on(TOKEN_EXPIRES_SOON, this.showWillTimeoutModal);
-        this.tokenExpirationChecker.on(TOKEN_HAS_EXPIRED, this.showDidTimeoutModal);
-        this.state = {
-            willTimeout: false,
-            didTimeout: false,
-        };
-    }
-
-    componentDidMount() {
-        this.tokenExpirationChecker.start();
     }
 
     componentWillUnmount() {
         this.props.resetValidation();
-        this.tokenExpirationChecker.destroy();
     }
-
-    showWillTimeoutModal = () => {
-        this.setState({ willTimeout: true });
-    };
-
-    showDidTimeoutModal = () => {
-        this.setState({
-            willTimeout: false,
-            didTimeout: true,
-        });
-        this.tokenExpirationChecker.pause();
-    };
-
-    saveAndLogin = () => {
-        this.props.saveAndLogin();
-    };
-
-    loginAndRedirect = () => {
-        loginWithRedirectToCurrentLocation();
-    };
 
     render() {
         const { ad, isNew, onPreviewAdClick } = this.props;
-        const { didTimeout, willTimeout } = this.state;
 
         // Fra EditHeader
         const limitedAccess = ad.createdBy !== 'pam-rekrutteringsbistand';
@@ -80,26 +40,6 @@ class Edit extends React.Component {
 
         return (
             <div className="Edit">
-                {willTimeout && (
-                    <SessionExpirationModal
-                        title={'Du blir snart logget ut'}
-                        bodyText={'Lagre nå for å unngå å miste endringene dine.'}
-                        mainButtonText={'Lagre'}
-                        mainOnClick={this.saveAndLogin}
-                        secondaryButtonText={'Avbryt'}
-                        secondaryOnClick={this.loginAndRedirect}
-                        isOpen={willTimeout}
-                    />
-                )}
-                {didTimeout && (
-                    <SessionExpirationModal
-                        title={'Du har blitt logget ut'}
-                        bodyText={'Denne sesjonen har utløpt.'}
-                        mainButtonText={'Logg inn'}
-                        mainOnClick={this.loginAndRedirect}
-                        isOpen={didTimeout}
-                    />
-                )}
                 <div className="Edit__actions">
                     <CandidateActions />
                     <div className="blokk-xs">
@@ -193,7 +133,6 @@ Edit.propTypes = {
     resetValidation: PropTypes.func.isRequired,
     isNew: PropTypes.bool,
     onPreviewAdClick: PropTypes.func.isRequired,
-    saveAndLogin: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = (state) => ({
@@ -201,7 +140,6 @@ const mapStateToProps = (state) => ({
 });
 
 const mapDispatchToProps = (dispatch) => ({
-    saveAndLogin: () => dispatch({ type: SAVE_AD, login: true }),
     resetValidation: () => dispatch({ type: RESET_VALIDATION_ERROR }),
 });
 
