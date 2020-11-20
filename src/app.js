e98cc129855b79fbcf0a0777c3ab327cf62d3777
Kjetil Svalestuen <kjetil.svalestuen@nav.no>
2020-11-20 09:06:15 +0100

    Fjern irrelevant endring

--- src/app.js
+++ src/app.js
@@ -93,7 +93,7 @@ const App = () => {
     useEffect(() => {
         dispatch({ type: FETCH_FEATURE_TOGGLES });
         dispatch({ type: FETCH_REPORTEE });
-    }, [dispatch]);
+    }, []);
 
     return (
         <main>
