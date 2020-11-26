1fd71c7df52025dac3aa1973ddc16091fa460f10
André Mossige <mossige.a@gmail.com>
2020-11-26 07:46:25 +0100

    Print tom string hvis kandidat er undefined

--- src/ad/kandidatModal/LeggTilKandidatModal.tsx
+++ src/ad/kandidatModal/LeggTilKandidatModal.tsx
@@ -39,7 +39,7 @@ type Props = {
     setNotat: (notat: string) => void;
 };
 
-type Kandidat = {
+export type Kandidat = {
     arenaKandidatnr: string;
     fornavn: string;
     etternavn: string;
