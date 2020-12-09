137b5ab90193960d63e57e33eedee31e719f7e01
Kjetil Svalestuen <kjesvale@users.noreply.github.com>
2020-12-09 10:44:39 +0100

    Fiks forlat stilling (#219)
    * Lag egen, isolert state for å forkaste ny stilling
    
    * Deploy til dev
    
    * Forbedre state og feilhåndtering under forkasting av ny stilling

--- src/ad/bekreft-forlat-siden-modal/BekreftForlatSidenModal.tsx
+++ src/ad/bekreft-forlat-siden-modal/BekreftForlatSidenModal.tsx
@@ -2,10 +2,14 @@ import React, { FunctionComponent } from 'react';
 import { connect } from 'react-redux';
 import { Hovedknapp } from 'nav-frontend-knapper';
 import { Normaltekst, Undertittel } from 'nav-frontend-typografi';
+import NavFrontendSpinner from 'nav-frontend-spinner';
+
+import { NyStillingState } from '../adReducer';
 import LinkButton from '../../common/linkbutton/LinkButton';
 import ModalMedStillingScope from '../../ModalMedStillingScope';
 import State from '../../State';
 import './BekreftForlatSidenModal.less';
+import { AlertStripeFeil } from 'nav-frontend-alertstriper';
 
 type Props = {
     vis: boolean;
@@ -13,9 +17,11 @@ type Props = {
     created: any;
     onBliPåSidenClick: () => void;
     onForlatSidenClick: () => void;
+    nyStillingState: NyStillingState;
 };
+
 const BekreftForlatSidenModal: FunctionComponent<Props> = (props) => {
-    const { vis, updated, created, onBliPåSidenClick, onForlatSidenClick } = props;
+    const { vis, updated, created, onBliPåSidenClick, onForlatSidenClick, nyStillingState } = props;
 
     return (
         <ModalMedStillingScope
@@ -37,10 +43,21 @@ const BekreftForlatSidenModal: FunctionComponent<Props> = (props) => {
                     Hvis du navigerer bort fra denne siden uten å lagre så mister du informasjonen.
                 </Normaltekst>
                 <Hovedknapp onClick={onBliPåSidenClick}>Bli på siden</Hovedknapp>
-                <LinkButton className="lenke" onClick={onForlatSidenClick}>
+                <LinkButton
+                    className="lenke BekreftForlatSidenModal__forlat-siden-knapp"
+                    onClick={onForlatSidenClick}
+                >
                     Forlat siden
+                    {nyStillingState === NyStillingState.Forkastes && (
+                        <NavFrontendSpinner className="BekreftForlatSidenModal__forkaster-stilling-spinner" />
+                    )}
                 </LinkButton>
             </div>
+            {nyStillingState === NyStillingState.Feil && (
+                <AlertStripeFeil className="BekreftForlatSidenModal__forkast-stilling-feil">
+                    Klarte ikke å forkaste den nye stillingen.
+                </AlertStripeFeil>
+            )}
         </ModalMedStillingScope>
     );
 };
@@ -49,6 +66,7 @@ const mapStateToProps = (state: State) => ({
     adStatus: state.adData.status,
     updated: state.adData.updated,
     created: state.adData.created,
+    nyStillingState: state.ad.nyStillingState,
 });
 
 export default connect(mapStateToProps)(BekreftForlatSidenModal);
