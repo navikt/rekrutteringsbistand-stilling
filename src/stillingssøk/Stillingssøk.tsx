7695caa514e2fd7c21e60295516c0db1d3b88706
Kjetil Svalestuen <kjesvale@users.noreply.github.com>
2021-01-22 13:39:02 +0100

    Test stillingssok proxy (#234)
    * Revert til history@4
    
    * Legg til ny proxy mot stillingssok-proxy
    
    * Lag enkel, ny stillingsside som går mot ny proxy
    
    * Pretty-print JSON
    
    * Deploy til dev
    
    * Ikke gjør tusen kall
    
    * Søk på alle stillinger
    
    * Enkelt GUI for søk
    
    * Bedre styling
    
    * Legg til muligheter som default choice

---
+++ src/stillingssøk/Stillingssøk.tsx
@@ -0,0 +1,160 @@
+import React, { ChangeEvent, FunctionComponent, useEffect, useState } from 'react';
+import { Select, Textarea } from 'nav-frontend-skjema';
+import { Element, Normaltekst, Sidetittel } from 'nav-frontend-typografi';
+import { fetchPost } from '../api/api';
+import './Stillingssøk.less';
+
+const stillingsøkProxy = '/rekrutteringsbistand-stilling/stillingssok-proxy';
+
+type Resultat = {
+    took: number;
+    timed_out: boolean;
+    hits: {
+        total: {
+            value: number;
+            relation: string;
+        };
+        max_score: number;
+        hits: Array<Hit>;
+    };
+};
+
+type Hit = {
+    _index: string;
+    _type: string;
+    _id: string;
+    _score: number;
+    _source: any;
+};
+
+const defaultQuery = {
+    query: {
+        match_all: {},
+    },
+};
+
+const tittelQuery = {
+    query: { match: { 'stilling.title': { query: 'Tittel' } } },
+};
+
+const tilretteleggingQuery = {
+    query: {
+        bool: {
+            must_not: [
+                {
+                    term: {
+                        'stilling.status': 'DELETED',
+                    },
+                },
+            ],
+            must: [
+                {
+                    term: {
+                        'stilling.source': 'DIR',
+                    },
+                },
+                {
+                    range: {
+                        'stilling.created': {
+                            gte: '2020-01-01',
+                            lt: '2022-01-01',
+                        },
+                    },
+                },
+            ],
+            should: [
+                {
+                    match_phrase: {
+                        'stilling.properties.tags': 'INKLUDERING',
+                    },
+                },
+                {
+                    match_phrase: {
+                        'stilling.properties.tags': 'PRIORITERT_MÅLGRUPPE',
+                    },
+                },
+                {
+                    match_phrase: {
+                        'stilling.properties.tags': 'TILTAK_ELLER_VIRKEMIDDEL',
+                    },
+                },
+            ],
+            minimum_should_match: 1,
+        },
+    },
+};
+
+const Stillingssøk: FunctionComponent = () => {
+    const [resultat, setResultat] = useState<Resultat>();
+    const [query, setQuery] = useState<object>(defaultQuery);
+    const [input, setInput] = useState<string>(formaterJson(query));
+    const [feil, setFeil] = useState<string | null>(null);
+
+    useEffect(() => {
+        console.log('Query:', formaterJson(query));
+        const hentStillinger = async () => {
+            try {
+                setResultat(await fetchPost(`${stillingsøkProxy}/_search`, query));
+            } catch (e) {
+                console.log('Klarte ikke å hente stillinger');
+            }
+        };
+
+        hentStillinger();
+    }, [query]);
+
+    const onQueryChange = (newInput: string) => {
+        setInput(newInput);
+
+        try {
+            const inputQuery = JSON.parse(newInput);
+            setQuery(inputQuery);
+            setFeil(null);
+        } catch (e) {
+            setFeil('Søket er ugyldig');
+        }
+    };
+
+    const onQuerySelect = (event: ChangeEvent<HTMLSelectElement>) => {
+        onQueryChange(event.target.value);
+    };
+
+    const stillinger = resultat?.hits.hits;
+
+    return (
+        <div className="stillingssøk">
+            <Sidetittel className="blokk-m">Nytt stillingssøk</Sidetittel>
+            <Select className="blokk-m" label="Velg en query" onChange={onQuerySelect}>
+                <option value={formaterJson(defaultQuery)}>Søk alle stillinger</option>
+                <option value={formaterJson(tittelQuery)}>Søk på tittel</option>
+                <option value={formaterJson(tilretteleggingQuery)}>
+                    Søk på tilretteleggingsmuligheter
+                </option>
+            </Select>
+
+            <div className="blokk-m">
+                <Textarea
+                    value={input}
+                    label="Søk på stillinger"
+                    onChange={(e) => onQueryChange(e.target.value)}
+                    feil={feil}
+                ></Textarea>
+            </div>
+
+            <Element>Resultater</Element>
+            {stillinger ? (
+                <ul>
+                    {stillinger.map((stilling) => (
+                        <Normaltekst tag="li">{stilling._source.stilling.title}</Normaltekst>
+                    ))}
+                </ul>
+            ) : (
+                <Normaltekst>Ingen stillinger</Normaltekst>
+            )}
+        </div>
+    );
+};
+
+const formaterJson = (json: Object) => JSON.stringify(json, null, 4);
+
+export default Stillingssøk;
