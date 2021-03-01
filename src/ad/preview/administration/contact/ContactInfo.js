c4ab1e77c94430e8a6a49a803845fa9fbfdab6ca
Mads Lee Giil <mads.lee.giil@nav.no>
2021-03-01 13:07:38 +0100

    Lag modal for å markere stilling som sin

--- src/ad/preview/administration/contact/ContactInfo.js
+++ src/ad/preview/administration/contact/ContactInfo.js
@@ -7,10 +7,19 @@ import { MARKER_SOM_MIN, SAVE_AD } from '../../../adReducer';
 import './ContactInfo.less';
 import { SET_NAV_IDENT, SET_REPORTEE } from '../../../adDataReducer';
 import { erDirektemeldtStilling } from '../../../adUtils';
+import MarkerSomMinModal from '../markerSomMinModal/MarkerSomMinModal';
 
 class ContactInfo extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            modalMarkerSomMinStillingErÅpen: false,
+        };
+    }
+
     onMarkerSomMinKlikkEksternStilling = () => {
         this.props.markerSomMin();
+        this.setState({ markerSomMinStillingModalErÅpen: false });
     };
 
     onMarkerSomMinKlikkInternStilling = () => {
@@ -18,52 +27,47 @@ class ContactInfo extends React.Component {
         setReportee(innlogget.displayName);
         setNavIdent(innlogget.navIdent);
         saveAd();
+        this.setState({ markerSomMinStillingModalErÅpen: false });
     };
 
     render() {
-        const { stilling, stillingsinfo, innlogget } = this.props;
+        const { stilling, stillingsinfo } = this.props;
         const isDir = stilling && erDirektemeldtStilling(stilling.source);
-        const hasStillingsinfo = stillingsinfo && stillingsinfo.eierNavident;
         const { reportee, navIdent } = stilling.administration;
 
-        return isDir ? (
+        return (
             <div className="ContactInfo__preview">
                 <Element>Spørsmål om stillingen?</Element>
-                <Normaltekst>
-                    Kontaktperson hos NAV: {reportee} {navIdent ? ` (${navIdent})` : ''}
-                </Normaltekst>
-                {innlogget && innlogget.navIdent !== stilling.administration.navIdent && (
-                    <Knapp
-                        className="button-marker_som_min"
-                        onClick={this.onMarkerSomMinKlikkInternStilling}
-                        mini
-                    >
-                        Marker som min
-                    </Knapp>
+
+                {isDir ? (
+                    <Normaltekst>
+                        Kontaktperson hos NAV: {reportee} {navIdent ? ` (${navIdent})` : ''}
+                    </Normaltekst>
+                ) : (
+                    <Normaltekst>
+                        Kontaktperson hos NAV: {stillingsinfo.eierNavn}{' '}
+                        {stillingsinfo.eierNavident ? ` (${stillingsinfo.eierNavident})` : ''}
+                    </Normaltekst>
                 )}
+
+                <Knapp
+                    className="button-marker_som_min"
+                    onClick={() => this.setState({ markerSomMinStillingModalErÅpen: true })}
+                    mini
+                >
+                    Marker som min
+                </Knapp>
+
+                <MarkerSomMinModal
+                    erÅpen={this.state.markerSomMinStillingModalErÅpen}
+                    onAvbryt={() => this.setState({ markerSomMinStillingModalErÅpen: false })}
+                    onMarkerSomMin={
+                        isDir
+                            ? this.onMarkerSomMinKlikkInternStilling
+                            : this.onMarkerSomMinKlikkEksternStilling
+                    }
+                />
             </div>
-        ) : (
-            <>
-                {hasStillingsinfo && (
-                    <div className="ContactInfo__preview">
-                        <Element>Spørsmål om stillingen?</Element>
-                        <Normaltekst>
-                            Kontaktperson hos NAV: {stillingsinfo.eierNavn}{' '}
-                            {stillingsinfo.eierNavident ? ` (${stillingsinfo.eierNavident})` : ''}
-                        </Normaltekst>
-                        {(!stillingsinfo.eierNavident ||
-                            (innlogget && stillingsinfo.eierNavident !== innlogget.navIdent)) && (
-                            <Knapp
-                                className="button-marker_som_min"
-                                onClick={this.onMarkerSomMinKlikkEksternStilling}
-                                mini
-                            >
-                                Marker som min
-                            </Knapp>
-                        )}
-                    </div>
-                )}
-            </>
         );
     }
 }
---
+++ src/ad/preview/administration/markerSomMinModal/MarkerSomMinModal.tsx
@@ -0,0 +1,48 @@
+import React, { FunctionComponent } from 'react';
+import NavFrontendModal from 'nav-frontend-modal';
+import { Normaltekst, Undertittel } from 'nav-frontend-typografi';
+import { Flatknapp, Hovedknapp } from 'nav-frontend-knapper';
+import './MarkerSomMinModal.less';
+
+type Props = {
+    erÅpen: boolean;
+    onAvbryt: () => void;
+    onMarkerSomMin: () => void;
+};
+
+const MarkerSomMinModal: FunctionComponent<Props> = ({ erÅpen, onAvbryt, onMarkerSomMin }) => {
+    return (
+        <>
+            <NavFrontendModal
+                onRequestClose={onAvbryt}
+                isOpen={erÅpen}
+                contentLabel="Marker stillingen som min"
+                portalClassName="rek-stilling"
+                className="MarkerSomMinModal"
+            >
+                <Undertittel className="blokk-s">Marker stillingen som min</Undertittel>
+                <Normaltekst className="blokk-l">
+                    Hvis du markerer stillingen som din, blir du eier av stillingen og tilhørende
+                    kandidatliste. Du vil ha ansvar for kontakt med arbeidsgiver, og kan dele CV-er
+                    med arbeidsgiveren. Handlingen kan ikke angres.
+                </Normaltekst>
+                <div>
+                    <Hovedknapp
+                        className="MarkerSomMinModal__bekreftKnapp"
+                        onClick={onMarkerSomMin}
+                    >
+                        Marker som min
+                    </Hovedknapp>
+                    <Flatknapp
+                        className="marker-som-min__avbryt knapp-små-bokstaver"
+                        onClick={onAvbryt}
+                    >
+                        Avbryt
+                    </Flatknapp>
+                </div>
+            </NavFrontendModal>
+        </>
+    );
+};
+
+export default MarkerSomMinModal;
