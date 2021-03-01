0e1eded5c76b2bd4c3e850808f0de48149e099e2
Mads Lee Giil <mads.lee.giil@nav.no>
2021-03-01 13:39:14 +0100

    Bruk modal for å markere stilling som min

--- src/ad/preview/administration/contact/ContactInfo.js
+++ src/ad/preview/administration/contact/ContactInfo.js
@@ -7,6 +7,7 @@ import { MARKER_SOM_MIN, SAVE_AD } from '../../../adReducer';
 import './ContactInfo.less';
 import { SET_NAV_IDENT, SET_REPORTEE } from '../../../adDataReducer';
 import { erDirektemeldtStilling } from '../../../adUtils';
+import MarkerSomMinModal from '../markerSomMinModal/MarkerSomMinModal';
 
 class ContactInfo extends React.Component {
     constructor(props) {
@@ -29,50 +30,67 @@ class ContactInfo extends React.Component {
         this.setState({ markerSomMinStillingModalErÅpen: false });
     };
 
+    kanMarkereStillingSomMin = () => {
+        if (isDir) {
+            return innlogget && innlogget.navIdent !== stilling.administration.navIdent;
+        } else {
+            return (
+                !stillingsinfo.eierNavident ||
+                (innlogget && stillingsinfo.eierNavident !== innlogget.navIdent)
+            );
+        }
+    };
+
     render() {
-        const { stilling, stillingsinfo, innlogget } = this.props;
+        const { stilling, stillingsinfo } = this.props;
         const isDir = stilling && erDirektemeldtStilling(stilling.source);
-        const hasStillingsinfo = stillingsinfo && stillingsinfo.eierNavident;
         const { reportee, navIdent } = stilling.administration;
 
-        return isDir ? (
+        const skalViseStillingsinfo = isDir || stillingsinfo;
+
+        if (skalViseStillingsinfo) {
+            return null;
+        }
+
+        return (
             <div className="ContactInfo__preview">
                 <Element>Spørsmål om stillingen?</Element>
-                <Normaltekst>
-                    Kontaktperson hos NAV: {reportee} {navIdent ? ` (${navIdent})` : ''}
-                </Normaltekst>
-                {innlogget && innlogget.navIdent !== stilling.administration.navIdent && (
-                    <Knapp
-                        className="button-marker_som_min"
-                        onClick={this.onMarkerSomMinKlikkInternStilling}
-                        mini
-                    >
-                        Marker som min
-                    </Knapp>
+
+                {isDir ? (
+                    <Normaltekst>
+                        Kontaktperson hos NAV: {reportee} {navIdent ? ` (${navIdent})` : ''}
+                    </Normaltekst>
+                ) : (
+                    <Normaltekst>
+                        Kontaktperson hos NAV: {stillingsinfo.eierNavn}{' '}
+                        {stillingsinfo.eierNavident ? ` (${stillingsinfo.eierNavident})` : ''}
+                    </Normaltekst>
+                )}
+
+                {this.kanMarkereStillingSomMin && (
+                    <>
+                        <Knapp
+                            className="button-marker_som_min"
+                            onClick={() => this.setState({ markerSomMinStillingModalErÅpen: true })}
+                            mini
+                        >
+                            Marker som min
+                        </Knapp>
+
+                        <MarkerSomMinModal
+                            erÅpen={this.state.markerSomMinStillingModalErÅpen}
+                            onAvbryt={() =>
+                                this.setState({ markerSomMinStillingModalErÅpen: false })
+                            }
+                            onMarkerSomMin={
+                                isDir
+                                    ? this.onMarkerSomMinKlikkInternStilling
+                                    : this.onMarkerSomMinKlikkEksternStilling
+                            }
+                        />
+                    </>
                 )}
             </div>
-        ) : (
-            <>
-                {hasStillingsinfo && (
-                    <div className="ContactInfo__preview">
-                        <Element>Spørsmål om stillingen?</Element>
-                        <Normaltekst>
-                            Kontaktperson hos NAV: {stillingsinfo.eierNavn}{' '}
-                            {stillingsinfo.eierNavident ? ` (${stillingsinfo.eierNavident})` : ''}
-                        </Normaltekst>
-                        {(!stillingsinfo.eierNavident ||
-                            (innlogget && stillingsinfo.eierNavident !== innlogget.navIdent)) && (
-                            <Knapp
-                                className="button-marker_som_min"
-                                onClick={this.onMarkerSomMinKlikkEksternStilling}
-                                mini
-                            >
-                                Marker som min
-                            </Knapp>
-                        )}
-                    </div>
-                )}
-            </>
         );
     }
 }
