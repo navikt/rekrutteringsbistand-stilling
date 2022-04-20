89b728fb9cb683c8d307fa5eebc5c1445125e0bc
Joar Aurdal <joar.aurdal@nav.no>
2022-04-20 10:25:28 +0200

    Send med arbeidsgiver ved post av stilling

--- src/opprett-ny-stilling/OpprettNyStilling.tsx
+++ src/opprett-ny-stilling/OpprettNyStilling.tsx
@@ -58,6 +58,7 @@ const OpprettNyStilling: FunctionComponent<Props> = ({ onClose }) => {
         dispatch({
             type: CREATE_AD,
             kategori: stillingskategori,
+            arbeidsgiver: arbeidsgiver,
         });
     };
 
