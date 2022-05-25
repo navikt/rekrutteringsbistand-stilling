d58a087609b8ee0ede7f9ebe6ad797ae9eefad3a
Kjetil Svalestuen <kjetil.svalestuen@nav.no>
2022-05-25 10:03:35 +0200

    Bytt til Amplitude EU

--- src/verktøy/amplitude.ts
+++ src/verktøy/amplitude.ts
@@ -3,8 +3,8 @@ import { getMiljø, Miljø } from './sentry';
 
 const getApiKey = () => {
     return getMiljø() === Miljø.ProdGcp
-        ? '3a6fe32c3457e77ce81c356bb14ca886'
-        : '55477baea93c5227d8c0f6b813653615';
+        ? 'a8243d37808422b4c768d31c88a22ef4'
+        : '6ed1f00aabc6ced4fd6fcb7fcdc01b30';
 };
 
 const client: AmplitudeClient = amplitudeJs.getInstance();
