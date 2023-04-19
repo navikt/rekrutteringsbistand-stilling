4658462db55aae67c7ffb5b69923817c09332d85
Joar Aurdal <joar.aurdal@nav.no>
2023-04-19 15:20:37 +0200

    Fjern css prefix

--- craco.config.js
+++ craco.config.js
@@ -2,27 +2,5 @@ const CracoLessPlugin = require('craco-less');
 const cssprefixer = require('postcss-prefix-selector');
 
 module.exports = {
-    style: {
-        postcss: {
-            plugins: [
-                cssprefixer({
-                    prefix: '.rek-stilling',
-                    exclude: ['html', 'body', '.rek-stilling', ':root'],
-                    transform: function (prefix, selector, prefixedSelector) {
-                        if (selector.startsWith('body ')) {
-                            return `body ${prefix} ${selector.slice(5)}`;
-                        } else if (selector.startsWith('html ')) {
-                            return `html ${prefix} ${selector.slice(5)}`;
-                        } else if (selector.startsWith('.rek-stilling ')) {
-                            return selector;
-                        } else if (selector.includes('modal')) {
-                            return selector;
-                        }
-                        return prefixedSelector;
-                    },
-                }),
-            ],
-        },
-    },
     plugins: [{ plugin: CracoLessPlugin }],
 };
