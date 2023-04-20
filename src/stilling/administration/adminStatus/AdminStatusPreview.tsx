df7d14ff6dcd98de94faf7fb2966e8448f5bf6ac
Joar Aurdal <joar.aurdal@nav.no>
2023-04-20 16:51:37 +0200

    Flytt AdminstatusPreview til nytt designsystem

---
+++ src/stilling/administration/adminStatus/AdminStatusPreview.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { BodyShort } from '@navikt/ds-react';
+import css from './AdminStatusPreview.module.css';
+
+const AdminStatusPreview = () => {
+    const administration = useSelector((state: any) => state.adData.administration);
+
+    return (
+        <div className={css.AdminStatusPreview}>
+            <BodyShort size="small">
+                <b>Registrert av: </b>
+                {administration.reportee || ''}
+                {administration.navIdent ? ` (${administration.navIdent})` : ''}
+            </BodyShort>
+        </div>
+    );
+};
+
+export default AdminStatusPreview;
