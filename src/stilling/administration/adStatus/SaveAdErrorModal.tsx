5116163cf5ab298e547fb8fe1924f624958f05db
Joar Aurdal <joar.aurdal@nav.no>
2023-04-21 15:12:02 +0200

    Flytt save error modal til nytt designsystem

---
+++ src/stilling/administration/adStatus/SaveAdErrorModal.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { HIDE_AD_SAVED_ERROR_MODAL } from '../../adReducer';
+import css from './SaveAdErrorModal.module.css';
+import { BodyLong, Button, Heading, Modal } from '@navikt/ds-react';
+
+const SaveAdErrorModal = () => {
+    const dispatch = useDispatch();
+
+    const validation = useSelector((state: any) => state.adValidation.errors);
+    const showAdSavedErrorModal = useSelector((state: any) => state.ad.showAdSavedErrorModal);
+
+    const onClose = () => {
+        dispatch({ type: HIDE_AD_SAVED_ERROR_MODAL });
+    };
+
+    return (
+        showAdSavedErrorModal && (
+            <Modal
+                open={showAdSavedErrorModal}
+                onClose={onClose}
+                closeButton
+                className={css.saveAdErrorModal}
+            >
+                <Heading level="2" size="small" spacing>
+                    Kan ikke lagre stillingen
+                </Heading>
+                <BodyLong>Stillingen kan ikke lagres før følgende feil er rettet:</BodyLong>
+                <BodyLong as="ul" spacing>
+                    {validation.title && (
+                        <li className="skjemaelement__feilmelding">{validation.title}</li>
+                    )}
+                    {validation.notat && (
+                        <li className="skjemaelement__feilmelding">{validation.notat}</li>
+                    )}
+                    {validation.styrk && (
+                        <li className="skjemaelement__feilmelding">{validation.styrk}</li>
+                    )}
+                    {validation.applicationEmail && (
+                        <li className="skjemaelement__feilmelding">
+                            {validation.applicationEmail}
+                        </li>
+                    )}
+                    {validation.contactPersonEmail && (
+                        <li className="skjemaelement__feilmelding">
+                            {validation.contactPersonEmail}
+                        </li>
+                    )}
+                    {validation.contactPersonPhone && (
+                        <li className="skjemaelement__feilmelding">
+                            {validation.contactPersonPhone}
+                        </li>
+                    )}
+                    {validation.postalCode && (
+                        <li className="skjemaelement__feilmelding">{validation.postalCode}</li>
+                    )}
+                </BodyLong>
+                <Button onClick={onClose}>Lukk</Button>
+            </Modal>
+        )
+    );
+};
+
+export default SaveAdErrorModal;
