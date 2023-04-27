84c579d99c51ddd5be95c9f96920992263399485
Kjetil Svalestuen <kjetil.svalestuen@nav.no>
2023-04-27 09:43:05 +0200

    Fjern rekbis-stilling-prefiks

--- src/common/ModalMedStillingScope.tsx
+++ src/common/ModalMedStillingScope.tsx
@@ -2,7 +2,7 @@ import React, { FunctionComponent } from 'react';
 import NavFrontendModal, { ModalProps } from 'nav-frontend-modal';
 
 const ModalMedStillingScope: FunctionComponent<ModalProps> = (props) => (
-    <NavFrontendModal {...props} portalClassName="rek-stilling"></NavFrontendModal>
+    <NavFrontendModal {...props}></NavFrontendModal>
 );
 
 export default ModalMedStillingScope;
--- src/index.tsx
+++ src/index.tsx
@@ -12,18 +12,12 @@ import { Provider } from 'react-redux';
 
 const skalEksporteres = process.env.REACT_APP_EXPORT || process.env.NODE_ENV === 'production';
 
-// Alle klassenavn blir prefikset med ".rek-stilling" i craco-configen, så også koden
-// som brukes under utvikling må wrappes i et element med dette klassenavnet.
-export const cssScopeForApp = 'rek-stilling';
-
 const AppMedRouter: FunctionComponent<AppProps> = (props) => (
-    <div className={cssScopeForApp}>
-        <Sentry.ErrorBoundary>
-            <Router navigator={props.history} location={props.history.location}>
-                <AppMedStore {...props} />
-            </Router>
-        </Sentry.ErrorBoundary>
-    </div>
+    <Sentry.ErrorBoundary>
+        <Router navigator={props.history} location={props.history.location}>
+            <AppMedStore {...props} />
+        </Router>
+    </Sentry.ErrorBoundary>
 );
 
 export const AppMedStore: FunctionComponent<AppProps> = ({ history, navKontor }) => (
