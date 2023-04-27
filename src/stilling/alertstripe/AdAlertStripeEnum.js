6f9332847559645f2a8890413bed844a4179f3bb
Kjetil Svalestuen <kjetil.svalestuen@nav.no>
2023-04-27 09:20:56 +0200

    Innfør nytt ds på Stillingssiden

--- src/stilling/alertstripe/AdAlertStripeEnum.js
+++
@@ -1,7 +0,0 @@
-const AdAlertStripeEnum = {
-    SAVED: 'SAVED',
-    PUBLISHED_CHANGES: 'PUBLISHED_CHANGES',
-    WILL_PUBLISH_CHANGES: 'WILL_PUBLISH_CHANGES',
-};
-
-export default AdAlertStripeEnum;
